3505bf3bbb7935f84fced84b0435ed0072db9aef
Мария <kuranova.marie@yandex.ru>
2021-10-21 21:26:16 +0300

    Немного подправила класс Предмет

--- data_model/matter_lesson.py
+++ data_model/matter_lesson.py
@@ -1,10 +1,15 @@
-class matter:
-    def __init__(self, name, id):
-        self.__name = name
-        self.__id = id
+from random import randrange
 
+
+class Matter:
+    def __init__(self, name):
+        self.__name = name
+        self.__id = randrange(1, 350000)
+
+    @property
     def get_id(self):
         return self.__id
 
+    @property
     def get_name(self):
         return self.__name
