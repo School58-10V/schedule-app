2d4e62af951e6019a6601b8cd8f687af56b8c01d
Mitya <you@example.com>
2021-10-21 21:09:25 +0300

    push smthg to team_R brunch

---
+++ data_model/Subject.py
@@ -0,0 +1,8 @@
+class a_series_of_lessons:
+    def __init__(self):
+        self.num_of_group =
+        self.teacher =
+        self.group_right_now =  # ласс, занимающийся в данный момент.(id группы/класса)
+        self.theme_of_lesson =
+        self.room =
+        self.time_of_a_lesson =
