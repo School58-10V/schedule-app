af2ca4f6ad8d68d6bfe677738ac3e7e1752faccf
DragonMadness_ <75204687+DragonMadness@users.noreply.github.com>
2021-10-25 20:07:11 +0300

    Обновляю расписание в соответствии с отзывами

--- data_model/timetable.py
+++ data_model/timetable.py
@@ -1,33 +1,12 @@
-import random
-
-
 class TimeTable:
-    def __init__(self):
-        self.__table_id = random.randrange(0, 350000)
-        self.__year = "0-0"
+    def __init__(self, year, timetable_id=None):
+        self.__table_id = timetable_id
+        self.__year = year
 
     @property
-    def getter(self):
-        return f"id расписания: {self.__table_id}, учебный год: {self.__year}"
-
-
-class Day:
-    def __init__(self):
-        self.__day_id = random.randrange(1, 345678987)
-        self.__lessons = {"class_id": ["lessons_id"]}
-
+    def table_id(self):
+        return self.__table_id
+    
     @property
-    def getter(self):
-        return f"id дня: {self.__day_id}, расписание на день: {self.__lessons}"
-
-
-class NoLearningPeriod:
-    def __init__(self):
-        self.__no_learning_period_id = random.randrange(1, 8400000)
-        self.__start_stop = "0.0.0-0.0.0"
-        self.__timetable_id = "получить откуда-то"
-
-    @property
-    def getter(self):
-        return f"id неучебного периода: {self.__no_learning_period_id}, время начала: {self.__start_stop}, id " \
-               f"расписания: {self.__timetable_id}"
+    def year(self):
+        return self.__year
