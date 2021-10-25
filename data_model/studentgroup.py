19dc2f4e93062511ecf46d7b79e884107364cdf4
khrom <khromov05@gmail.com>
2021-10-25 21:15:45 +0300

    Сделал класс TeachersOnLessonRows, Пофиксил остальные классы по заметкам, удалил класс класса и оставил класс группы.

--- data_model/studentgroup.py
+++ data_model/studentgroup.py
@@ -1,11 +1,11 @@
-class Student__Group:
-    # TODO: разве здесь правильно использовать нижние подчеркивания? но и камел касе тоже такое себе
+class StudentInGroup:
     """
         Класс ученика в группе. Используется для m2m отношения между
         Group и Student
     """
 
-    def __init__(self, student_id: int, group_id: int):
+    def __init__(self, student_group_id: int, student_id: int, group_id: int):
+        self.__student_group_id = student_group_id
         self.__student_id = student_id
         self.__group_id = group_id
 
@@ -14,3 +14,6 @@ class Student__Group:
 
     def get_group_id(self):
         return self.__group_id
+
+    def get_student_group_id(self):
+        return self.__student_group_id
