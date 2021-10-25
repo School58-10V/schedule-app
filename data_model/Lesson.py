fd206d0a4b807b6b085f94c71bf3d41b88cf7020
марина( <rinamgame@gmail.com>
2021-10-25 21:13:03 +0300

    cделала инит

--- data_model/Lesson.py
+++ data_model/Lesson.py
@@ -1,21 +1,21 @@
-
 class Lesson:
-    def __init__(self, ):
-        self.__start_time =
-        self.__end_time =
-        self.__day =
-        self.__teacher_id =  #замена
-        self.__lesson_id =
-        self.__group_for_lesson_id =
-        self.__subject =
+    def __init__(self, start_time: int, end_time: int, day: int, teacher_id: int, lesson_id: int, group_id: int,
+                 subject_id: int, notes: str):
+        self.__start_time = start_time
+        self.__end_time = end_time
+        self.__day = day
+        self.__teacher_id = teacher_id
+        self.__lesson_id = lesson_id
+        self.__group_id = group_id
+        self.__subject_id = subject_id
         self.__state = True
-        self.__notes =
+        self.__notes = notes
 
     def change_state(self):
         self.__state = not self.__state
 
     def get_time(self):
-        return self.__time
+        return self.__start_time, self.__end_time
 
     def get_day(self):
         return self.__day
@@ -27,10 +27,10 @@ class Lesson:
         return self.__lesson_id
 
     def get_group_for_lesson_id(self):
-        return self.__group_for_lesson_id
+        return self.__group_id
 
     def get_subject(self):
-        return self.__subject
+        return self.__subject_id
 
     def get_state(self):
         return self.__state
