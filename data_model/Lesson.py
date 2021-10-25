9f9c4540b67109da5f9654a0a5033d93cb78468a
Mitya <you@example.com>
2021-10-25 21:03:00 +0300

    push smthg to team_R brunch

--- data_model/Lesson.py
+++ data_model/Lesson.py
@@ -1,42 +1,39 @@
 
 class Lesson:
-    def __init__(self):
-        self.__time =
+    def __init__(self, ):
+        self.__start_time =
+        self.__end_time =
         self.__day =
-        self.__teacher_id =
+        self.__teacher_id =  #замена
         self.__lesson_id =
         self.__group_for_lesson_id =
         self.__subject =
-        self.__hometask =  # not necessary?
         self.__state = True
         self.__notes =
 
     def change_state(self):
-        self.state = not self.state
+        self.__state = not self.__state
 
-    def __get_time(self):
+    def get_time(self):
         return self.__time
 
-    def __get_day(self):
+    def get_day(self):
         return self.__day
 
-    def __get_teacher_id(self):
+    def get_teacher_id(self):
         return self.__teacher_id
 
-    def __get_lesson_id(self):
+    def get_lesson_id(self):
         return self.__lesson_id
 
-    def __get_group_for_lesson_id(self):
+    def get_group_for_lesson_id(self):
         return self.__group_for_lesson_id
 
-    def __get_subject(self):
+    def get_subject(self):
         return self.__subject
 
-    def __get_hometask(self):
-        return self.__hometask
-
-    def __get_state(self):
+    def get_state(self):
         return self.__state
 
-    def __get_notes(self):
+    def get_notes(self):
         return self.__notes
---
+++ data_model/LessonRow.py
@@ -0,0 +1,37 @@
+class LessonRow:
+    def __init__(self, start_time: int, end_time: int, group_id: int, theme_of_lesson: str, room_id: int):
+        self.__start_time = start_time # start time of lessons (9:00)
+        self.__end_time = end_time # end time of lessons (10:00)
+        self.__group_id =  group_id# класс, занимающийся в данный момент.(id группы/класса)
+        self.__theme_of_lesson = theme_of_lesson # Math. Russian language(id)
+        self.__room_id = room_id # id
+        self.__timetable_id =  #id
+        self.__num_of_studying_hours =
+        self.__lessonRow_id =
+
+    def get_num_of_group_id(self):
+        return self.__num_of_group
+
+    def get_teacher_id(self):
+        return self.__teacher
+
+    def get_group_right_now_id(self):
+        return self.__group_id
+
+    def get_theme_of_lesson_id(self):
+        return self.__theme_of_lesson
+
+    def get_room_id(self):
+        return self.__room
+
+    def get_start_time(self):
+        return self.__start_time
+
+    def get_end_time(self):
+        return self.__end_time
+
+    def get_num_of_studying_hours(self):
+        return self.__num_of_studying_hours
+
+    def ret_lessonRow_id(self):
+        return self.__lessonRow_id
