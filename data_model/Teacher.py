ced66412c2bf5604dad822e4147ddea6c080c7c3
Kaifec <Vladplanshe@gmail.com>
2021-10-25 21:10:13 +0300

    Добавил гетеры.

--- data_model/Teacher.py
+++ data_model/Teacher.py
@@ -1,5 +1,12 @@
+# fio - ФИО, что впринципе логично
+# teacher_id - ид учителя
+# bio - инфа о учителе
+# contacts - Контакты учителя
+# office_id - закреплённый кабинет
+# lesson - сделайте m2m связь, т.к. не знаю, не умею
+
 class Teacher:
-    def __init__(self, fio, teacher_id, bio, contacts, office_id, lesson):
+    def __init__(self, fio, teacher_id, bio, contacts, lesson, office_id=None):
         self.__fio = fio
         self.__teacher_id = teacher_id
         self.__bio = bio
@@ -7,9 +14,20 @@ class Teacher:
         self.__office_id = office_id
         self.__lesson = lesson
 
-# fio - ФИО, что впринципе логично
-# teacher_id - ид учителя
-# bio - инфа о учителе
-# contacts - Контакты учителя
-# office_id - закреплённый кабинет
-# lesson - сделайте m2m связь, т.к. не знаю, не умею
+    def get_fio(self):
+        return self.__fio
+
+    def get_teacher_id(self):
+        return self.__teacher_id
+
+    def get_bio(self):
+        return self.__bio
+
+    def get_contacts(self):
+        return self.__contacts
+
+    def get_lesson(self):
+        return self.__lesson
+
+    def get_office_id(self):
+        return self.__office_id
