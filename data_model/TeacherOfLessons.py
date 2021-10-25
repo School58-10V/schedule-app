b10e21706bbed954cbb65ddd0ad298a16fd12a23
ReCret <suskat22@gmail.com>
2021-10-25 22:17:17 +0300

    учитель по предметам

---
+++ data_model/TeacherOfLessons.py
@@ -0,0 +1,7 @@
+'''py
+class TeacherOfLessons:
+    def __init__(self, lesson_id, teacher_id, teacher_of_lesson_id):
+        self.__lesson_id = lesson_id
+        self.__teacher_id = teacher_id
+        self.__teacher_of_lesson_id = teacher_of_lesson_id
+'''
