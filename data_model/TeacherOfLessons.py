8e1b3a4cd1203fcedae689cd0923c8e7f699af89
ReCret <suskat22@gmail.com>
2021-10-28 20:01:38 +0300

    добавлены типы данных

--- data_model/TeacherOfLessons.py
+++ data_model/TeacherOfLessons.py
@@ -1,7 +1,5 @@
-'''py
 class TeacherOfLessons:
-    def __init__(self, lesson_id, teacher_id, teacher_of_lesson_id):
+    def __init__(self, lesson_id: int, teacher_id: int, teacher_of_lesson_id: int):
         self.__lesson_id = lesson_id
         self.__teacher_id = teacher_id
         self.__teacher_of_lesson_id = teacher_of_lesson_id
-'''
