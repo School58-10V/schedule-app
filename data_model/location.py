9663c97f6b198d0d4fcc9f376cb3ca4c81a04243
Eneev <ivan.eneev@tandex.ru>
2021-10-31 16:53:13 +0300

    Долонения к классу Location по теме save/parse.

--- data_model/location.py
+++ data_model/location.py
@@ -5,15 +5,19 @@
 ##             Link - на случай дистанта ссылка(в Сибирь) для подключения к месту проведения урока
 ## type_of_location - Тип локации- класс, поточная аудитория, видеоконференция и т.д.
 import json
+from __future__ import annotations
 
-class location:
+
+class Location:
     def __init__(self, type_of_location: str, location_id: int = None, num_of_class: int = None, profile: str = None,
-                 equipment: list = None, link: str = "Offline"):
+                 equipment: list = None, comment: str = None, location_desc: str = None, link: str = "Offline"):
         self.__location_id = location_id
         self.__num_of_class = num_of_class
         self.__profile = profile
         self.__equipment = equipment
         self.__link = link
+        self.__comment = comment
+        self.__location_desc = location_desc
         self.__type_of_location = type_of_location
 
     def get__location_id(self):
@@ -34,11 +38,43 @@ class location:
     def get_type_of_location(self):
         return self.__type_of_location
 
-    def save(self):
-        with open("./db/locations.json", mode="w") as data_file:
-            json.dump({"location_id": self.__location_id,
+    def __serialize_to_json(self):
+        return json.dumps({"location_id": self.__location_id,
                        "num_of_class": self.__num_of_class,
                        "profile": self.__profile,
                        "equipment": self.__equipment,
                        "link": self.__link,
-                       "type_of_location": self.__type_of_location}, data_file)
+                       "type_of_location": self.__type_of_location}, ensure_ascii=False)
+
+    def save(self, path="./db/locations.json"):
+        with open(path, mode="w", encoding='utf-8') as data_file:
+            data_file.write(self.__serialize_to_json())
+
+    def __str__(self):
+        return f'Location(type_of_location={self.__type_of_location}, name={self.__location_desc}, ' \
+               f'link={self.__link}, comment={self.__comment})'
+
+    @staticmethod
+    def parse(file_location) -> List[(Optional[str], Optional[Location])]:
+        f = open(file_location, encoding='utf-8')
+        lines = f.read().split('\n')[1:]
+        lines = [i.split(';') for i in lines]
+        res = []
+        for i in lines:
+            try:
+                location_type = i[0]
+                name = i[1]
+                link = i[2]
+                comment = i[3]
+                res.append((None, Location(location_type, link=link,
+                                           location_desc=name if name.isdigit() else None, comment=comment)))
+            except IndexError as e:
+                exception_text = f"Строка {lines.index(i) + 2} не добавилась в [res]"
+                print(exception_text)
+                print(e)
+                res.append((exception_text, None))
+            except Exception as e:
+                exception_text = f"Неизвестная ошибка в Location.parse():\n{e}"
+                print(exception_text)
+                res.append((exception_text, None))
+        return res
