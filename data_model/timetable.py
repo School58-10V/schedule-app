712eb864f822300bc233a0cd70a1db73e1418667
DragonMadness_ <supervasya45@gmail.com>
2021-10-31 22:38:36 +0300

    Переписал сохранение и сериализацию, подправил parse()

--- data_model/timetable.py
+++ data_model/timetable.py
@@ -18,18 +18,31 @@ class TimeTable:
     def __str__(self):
         return f"Timetable(table_id={self.__table_id}, year={self.__year})"
 
-    def __serialize_to_json(self):
-        return json.dumps({"table_id": self.__table_id,
-                           "year": self.__year}, ensure_ascii=False)
+    def __serialize_to_json(self, records: list) -> str:
+        # Добавляем новый объект в список
+        records.append({"subject_id": self.__table_id,
+                        "subject_name": self.__year})
 
-    def save(self, folder: str = 'tests/saves'):
-        with open(f"../{folder}/TimeTable.json", mode="w", encoding='utf-8') as data_file:
-            data_file.write(self.__serialize_to_json())
+        return json.dumps(records, ensure_ascii=False, indent=4)
+
+    def save(self, folder: str = 'db'):
+        record = []
+        try:
+            data_file = open(f"../{folder}/TimeTable.json", mode="r", encoding='utf-8')
+            record = json.loads(data_file.read())
+            data_file.close()
+            data_file = open(f"../{folder}/TimeTable.json", mode="w", encoding='utf-8')
+        except FileNotFoundError:
+            data_file = open(f"../{folder}/TimeTable.json", mode="w", encoding='utf-8')
+        except json.decoder.JSONDecodeError:
+            data_file = open(f"../{folder}/TimeTable.json", mode="w", encoding='utf-8')
+            record = []
+        data_file.write(self.__serialize_to_json(record))
 
     @staticmethod
     def parse(file_timetable) -> List[(Optional[str], Optional[TimeTable])]:
         f = open(file_timetable, encoding='utf-8')
-        lines = f.read().split('\n')
+        lines = f.read().split('\n')[1:]
         lines = [i.split(';') for i in lines]
         res = []
 
