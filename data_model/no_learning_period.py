3507fb2a70572f02d4cea8ba13775d828de79917
ReCret <suskat22@gmail.com>
2021-11-01 19:23:10 +0300

    добавлены новые методы

--- data_model/no_learning_period.py
+++ data_model/no_learning_period.py
@@ -1,3 +1,4 @@
+import json
 from datetime import date
 
 
@@ -6,7 +7,7 @@ class NoLearningPeriod:
                  no_learning_period_id: int = None):
         # Для начала и конца каникул можно использовать только дату
         self.__no_learning_period_id = no_learning_period_id
-        self.__star_time = start
+        self.__start_time = start
         self.__stop_time = stop
         self.__timetable_id = timetable_id
 
@@ -14,10 +15,48 @@ class NoLearningPeriod:
         return self.__no_learning_period_id
 
     def get_star_time(self) -> date:
-        return self.__star_time
+        return self.__start_time
 
     def get_stop_time(self) -> date:
         return self.__stop_time
 
     def get_timetable_id(self) -> int:
         return self.__timetable_id
+
+    def __serialize_to_json(self):
+        return json.dumps({"timetable_id": self.__timetable_id,
+                       "start": self.__start_time,
+                       "stop": self.__stop_time,
+                       "no_learning_period_id": self.__no_learning_period_id}, ensure_ascii=False)
+
+    def save(self, path="./db/no_learning_periods.json"):
+        with open(path, mode="w", encoding='utf-8') as data_file:
+            data_file.write(self.__serialize_to_json())
+
+    def __str__(self):
+        return f'NoLearningPeriod(timetable_id={self.__timetable_id}, start={self.__start_time}, ' \
+               f'stop={self.__stop_time}, no_learning_period_id={self.__no_learning_period_id})'
+
+    @staticmethod
+    def parse(file_no_learning_period) -> List[(Optional[str], Optional[NoLearningPeriod])]:
+        f = open(file_no_learning_period, encoding='utf-8')
+        lines = f.read().split('\n')[1:]
+        lines = [i.split(';') for i in lines]
+        res = []
+        for i in lines:
+            try:
+                timetable_id = i[0]
+                start = i[1]
+                stop = i[2]
+                no_learning_period_id = i[3]
+                res.append((None, NoLearningPeriod(timetable_id, start, stop, no_learning_period_id)))
+            except IndexError as e:
+                exception_text = f"Строка {lines.index(i) + 2} не добавилась в [res]"
+                print(exception_text)
+                print(e)
+                res.append((exception_text, None))
+            except Exception as e:
+                exception_text = f"Неизвестная ошибка в NoLearningPeriod.parse():\n{e}"
+                print(exception_text)
+                res.append((exception_text, None))
+        return res
