1f52fbff65b55b1da9ab847d881a5876a5287c5f
Olga Bugaenko <olgahaipriv7177@gmail.com>
2021-11-01 17:05:40 +0300

    изменённый Teacher.py

--- data_model/Teacher.py
+++ data_model/Teacher.py
@@ -31,3 +31,48 @@ class Teacher:
 
     def get_office_id(self):
         return self.__office_id
+
+    @staticmethod
+    def parse(file_location) -> List[(Optional[str], Optional[Location])]:
+        f = open(file_location, encoding='utf-8')
+        lines = f.read().split('\n')[1:]
+        lines = [i.split(';') for i in lines]
+        res = []
+
+        for i in lines:
+            try:
+                fio = i[0]
+                teacher_id = i[1]
+                subject = i[2]
+                office_id = i[3]
+                bio = i[4]
+                contacts = i[5]
+
+                res.append(
+                    (None, Teacher(fio, teacher_id, subject, office_id, bio, contacts)))
+            except IndexError as e:
+                exception_text = f"Строка {lines.index(i) + 2} не добавилась в [res]"
+                print(exception_text)
+                print(e)
+                res.append((exception_text, None))
+            except Exception as e:
+                exception_text = f"Неизвестная ошибка в Teacher.parse():\n{e}"
+                print(exception_text)
+                res.append((exception_text, None))
+
+        return res
+
+    def __str__(self):
+        return f'Lesson(fio = {self.__fio}, subject = {self.__subject}, bio = {self.__bio}, contacts =  {self.__contacts}) '
+
+    def __serialize_to_json(self):
+        return json.dumps({"start_time": self.__fio,
+                           "end_time": self.__teacher_id,
+                           "group_id": self.__bio,
+                           "subject_id": self.__contacts,
+                           "room_id": self.__office_id,
+                           "timetable_id": self.__subject}, ensure_ascii=False)
+
+    def save(self, file_way="./db/teacher.json"):
+        with open(file_way, mode="w", encoding='utf-8') as data_file:
+            data_file.write(self.__serialize_to_json())
