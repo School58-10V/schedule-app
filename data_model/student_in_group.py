e33de574cd886acbf458f18cfe6d4030f2f88d0b
Mitya <you@example.com>
2021-11-01 21:17:16 +0300

    push smthg to parse-save/team_R brunch

--- data_model/student_in_group.py
+++ data_model/student_in_group.py
@@ -1,4 +1,10 @@
-from typing import Optional
+# student_id - айди учиника
+# group_id - айди группы
+# student_group_id - айди группы учиников
+from __future__ import annotations  # нужно чтобы parse мог быть типизирован
+import json
+
+from typing import Optional, List
 
 
 class StudentInGroup:
@@ -7,16 +13,53 @@ class StudentInGroup:
         Group и Student
     """
 
-    def __init__(self, student_id: int, group_id: int, student_group_id: Optional[int] = None):
+    def __init__(self, student_id: int, group_id: int, student_group_id: int = None):
         self.__student_id = student_id
         self.__group_id = group_id
         self.__student_group_id = student_group_id
 
-    def get_student_id(self) -> int:
+    def get_student_id(self):
         return self.__student_id
 
-    def get_group_id(self) -> int:
+    def get_group_id(self):
         return self.__group_id
 
-    def get_student_group_id(self) -> Optional[int]:
+    def get_student_group_id(self):
         return self.__student_group_id
+
+    @staticmethod
+    def parse(file_location) -> List[(Optional[str], Optional[StudentInGroup])]:
+        f = open(file_location, encoding='utf-8')
+        lines = f.read().split('\n')[1:]
+        lines = [i.split(';') for i in lines]
+        res = []
+
+        for i in lines:
+            try:
+                student_id = i[0]
+                group_id = i[1]
+                student_group_id = i[2]
+                res.append((None, StudentInGroup(student_id, group_id, student_group_id)))
+            except IndexError as e:
+                exception_text = f"Строка {lines.index(i) + 2} не добавилась в [res]"
+                print(exception_text)
+                print(e)
+                res.append((exception_text, None))
+            except Exception as e:
+                exception_text = f"Неизвестная ошибка в Student_in_group.parse():\n{e}"
+                print(exception_text)
+                res.append((exception_text, None))
+
+        return res
+
+    def __str__(self):
+        return f''
+
+    def __serialize_to_json(self):
+        return json.dumps({'student_id': self.__student_id,
+                           'group_id': self.__group_id,
+                           'student_group_id': self.__student_group_id}, ensure_ascii=False)
+
+    def save(self, file_way="./db/student_in_group.json"):
+        with open(file_way, mode="w", encoding='utf-8') as data_file:
+            data_file.write(self.__serialize_to_json())
