f364ee758eddfd26f41f590dc163eeab4b51e171
Kaifec <Vladplanshe@gmail.com>
2021-11-01 21:12:10 +0300

    Убрал не нужный id

--- data_model/lesson_row.py
+++ data_model/lesson_row.py
@@ -17,12 +17,12 @@ class LessonRow:
     def __init__(self, group_id: int, subject_id: int, room_id: int, start_time: int, end_time: int,
                  timetable_id: int, lesson_row_id: int = None):
         self.__count_studying_hours = None
-        self.__start_time = start_time
-        self.__end_time = end_time
-        self.__group_id = group_id
-        self.__subject_id = subject_id
-        self.__room_id = room_id
-        self.__timetable_id = timetable_id  
+        self.__start_time = start_time  # start time of lessons (9:00)
+        self.__end_time = end_time  # end time of lessons (10:00)
+        self.__group_id = group_id  # класс, занимающийся в данный момент.(id группы/класса)
+        self.__subject_id = subject_id  # Math. Russian language(id)
+        self.__room_id = room_id  # id
+        self.__timetable_id = timetable_id  # id
         self.__lesson_row_id = lesson_row_id
 
     def count_studying_hours(self):
@@ -56,8 +56,7 @@ class LessonRow:
                            "room_id": self.__room_id,
                            "start_time": self.__start_time,
                            "end_time": self.__end_time,
-                           "timetable_id": self.__timetable_id,
-                           "lesson_row_id": self.__lesson_row_id}, ensure_ascii=False)
+                           "timetable_id": self.__timetable_id}, ensure_ascii=False)
 
     def save(self, path="./db/lesson_row.json"):
         with open(path, mode="w", encoding='utf-8') as data_file:
@@ -65,8 +64,7 @@ class LessonRow:
 
     def __str__(self):
         return f'LessonRow(count_studying_hours={self.__count_studying_hours}, group_id={self.__group_id}, subject_id={self.__subject_id} ' \
-               f'room_id={self.__room_id}), start_timee={self.__start_time}), end_time={self.__end_time}), timetable_id={self.__timetable_id}), ' \
-               f'lesson_row_id={self.__lesson_row_id}) '
+               f'room_id={self.__room_id}), start_time={self.__start_time}, end_time={self.__end_time}), timetable_id={self.__timetable_id}), '
 
     @staticmethod
     def parse(file_location) -> List[(Optional[str], Optional[LessonRow])]:
@@ -83,10 +81,10 @@ class LessonRow:
                 start_time = i[4]
                 end_time = i[5]
                 timetable_id = i[6]
-                lesson_row_id = i[7]
 
-                res.append((None, LessonRow(int(count_studying_hours), int(group_id), int(subject_id), int(room_id), int(start_time), int(end_time), int(timetable_id),
-                                            int(lesson_row_id))))
+                res.append((None, LessonRow(int(count_studying_hours), int(group_id), int(subject_id), int(room_id), int(start_time),
+                                            int(end_time), int(timetable_id))))
+
             except IndexError as e:
                 exception_text = f"Строка {lines.index(i) + 2} не добавилась в [res]"
                 print(exception_text)
