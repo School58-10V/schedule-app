46732bcfeea3bce93de7538a04af22c41f5a1d45
VeryBigSad <khromov05@gmail.com>
2021-11-01 19:40:12 +0300

    добавил функции save(), parse(), __serialize_to_json() и __str__() в класс Group()

--- data_model/group.py
+++ data_model/group.py
@@ -1,10 +1,14 @@
-from typing import Optional
+from __future__ import annotations  # нужно чтобы parse мог быть типизирован
+
+import json
+from typing import Optional, List
 
 
 class Group:
     """
         Класс группы.
     """
+
     def __init__(
             self, teacher_id: int, class_letter: str, grade: int,
             profile_name: str, group_id: Optional[int] = None
@@ -29,3 +33,47 @@ class Group:
 
     def get_id(self) -> Optional[int]:
         return self.__group_id
+
+    def __str__(self) -> str:
+        return f'Group(teacher_id={self.__teacher_id}, class_letter={self.__class_letter}, ' \
+               f'grade={self.__grade}, profile_name={self.__profile_name}, group_id={self.__group_id})'
+
+    def __serialize_to_json(self) -> str:
+        return json.dumps({"teacher_id": self.__teacher_id,
+                           "class_letter": self.__class_letter,
+                           "grade": self.__grade,
+                           "profile_name": self.__profile_name,
+                           "group_id": self.__group_id},
+                          ensure_ascii=False)
+
+    @staticmethod
+    def parse(file_location) -> List[(Optional[str], Optional[Group])]:
+        f = open(file_location, encoding='utf-8')
+        lines = f.read().split('\n')[1:]
+        lines = [i.split(';') for i in lines]
+        res = []
+        for i in lines:
+            try:
+                teacher_id = i[0]
+                class_letter = i[1]
+                grade = i[2]
+                profile_name = i[3]
+                group_id = i[4]
+                res.append((None, Group(teacher_id, class_letter, grade, profile_name, group_id)))
+
+            except IndexError as e:
+                exception_text = f"Строка {lines.index(i) + 2} не добавилась в [res]"
+                print(exception_text)
+                print(e)
+                res.append((exception_text, None))
+            except Exception as e:
+                exception_text = f"Неизвестная ошибка в Group.parse():\n{e}"
+                print(exception_text)
+                res.append((exception_text, None))
+        return res
+
+    def save(self, output_path: str = "db"):
+        with open(f'{output_path}/group.json', mode="a", encoding='utf-8') as data_file:
+            if data_file.read() != '':
+                data_file.write('\n')
+            data_file.write(self.__serialize_to_json())
