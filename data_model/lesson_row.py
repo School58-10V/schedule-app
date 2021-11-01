cd5ba7fc5da3782de33bfbaa9f051fd69b3c04ba
Kaifec <Vladplanshe@gmail.com>
2021-11-01 20:02:44 +0300

    Сделал для lesson_row - save, __str__, parse, __serialize_to_json

--- data_model/lesson_row.py
+++ data_model/lesson_row.py
@@ -6,9 +6,17 @@
 # timetable_id год в который происходят уроки
 # lesson_row_id айди самого класса ряд уроков
 
+from __future__ import annotations
+
+import json
+
+from typing import Optional, List
+
+
 class LessonRow:
     def __init__(self, start_time: int, end_time: int, group_id: int, subject_id: int, room_id: int,
                  timetable_id: int, lesson_row_id: int = None):
+        self.__count_studying_hours = None
         self.__start_time = start_time  # start time of lessons (9:00)
         self.__end_time = end_time  # end time of lessons (10:00)
         self.__group_id = group_id  # класс, занимающийся в данный момент.(id группы/класса)
@@ -40,3 +48,50 @@ class LessonRow:
 
     def get_lesson_row_id(self) -> int:
         return self.__lesson_row_id
+
+    def __serialize_to_json(self):
+        return json.dumps({"count_studying_hours": self.__count_studying_hours,
+                           "group_id": self.__group_id,
+                           "subject_id": self.__subject_id,
+                           "room_id": self.__room_id,
+                           "end_time": self.__end_time,
+                           "timetable_id": self.__timetable_id,
+                           "lesson_row_id": self.__lesson_row_id}, ensure_ascii=False)
+
+    def save(self, path="./db/lesson_row.json"):
+        with open(path, mode="w", encoding='utf-8') as data_file:
+            data_file.write(self.__serialize_to_json())
+
+    def __str__(self):
+        return f'LessonRow(count_studying_hours={self.__count_studying_hours}, group_id={self.__group_id}, subject_id={self.__subject_id} ' \
+               f'room_id={self.__room_id}), end_time={self.__end_time}), timetable_id={self.__timetable_id}), ' \
+               f'lesson_row_id={self.__lesson_row_id}) '
+
+    @staticmethod
+    def parse(file_location) -> List[(Optional[str], Optional[LessonRow])]:
+        f = open(file_location, encoding='utf-8')
+        lines = f.read().split('\n')[1:]
+        lines = [i.split(';') for i in lines]
+        res = []
+        for i in lines:
+            try:
+                count_studying_hours = i[0]
+                group_id = i[1]
+                subject_id = i[2]
+                room_id = i[3]
+                end_time = i[4]
+                timetable_id = i[5]
+                lesson_row_id = i[6]
+
+                res.append((None, LessonRow(int(count_studying_hours), int(group_id), int(subject_id), int(room_id), int(end_time), int(timetable_id),
+                                            int(lesson_row_id))))
+            except IndexError as e:
+                exception_text = f"Строка {lines.index(i) + 2} не добавилась в [res]"
+                print(exception_text)
+                print(e)
+                res.append((exception_text, None))
+            except Exception as e:
+                exception_text = f"Неизвестная ошибка в Teacher.parse():\n{e}"
+                print(exception_text)
+                res.append((exception_text, None))
+        return res
