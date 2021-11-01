d2056d653e931c0e5b3f1bc7e69490bc3df75670
Mitya <you@example.com>
2021-11-01 21:08:56 +0300

    push smthg to parse-save/team_R brunch

--- data_model/student.py
+++ data_model/student.py
@@ -1,5 +1,14 @@
-from datetime import date
-from typing import Optional
+from __future__ import annotations  # нужно чтобы parse мог быть типизирован
+import json
+
+from typing import Optional, List
+
+
+# full name - полное имя студента
+# date_of_birth - дата рождения ученика
+# student_id - id студента
+# contacts - контакты родителей ученика
+# bio - биография студента
 
 
 class Student:
@@ -8,8 +17,8 @@ class Student:
     """
 
     def __init__(
-            self, full_name: str, date_of_birth: date, student_id: Optional[int] = None,
-            contacts: Optional[str] = None, bio: Optional[str] = None
+            self, full_name: str, date_of_birth: str, student_id: int = None,
+            contacts: str = None, bio: str = None
     ):
         self.__full_name = full_name
         self.__date_of_birth = date_of_birth
@@ -17,17 +26,60 @@ class Student:
         self.__contacts = contacts
         self.__bio = bio
 
-    def get_full_name(self) -> str:
+    def get_full_name(self):
         return self.__full_name
 
-    def get_date_of_birth(self) -> date:
+    def get_date_of_birth(self):
         return self.__date_of_birth
 
-    def get_id(self) -> Optional[int]:
+    def get_id(self):
         return self.__student_id
 
-    def get_contacts(self) -> Optional[str]:
+    def get_contacts(self):
         return self.__contacts
 
-    def get_bio(self) -> Optional[str]:
+    def get_bio(self):
         return self.__bio
+
+    @staticmethod
+    def parse(file_location) -> List[(Optional[str], Optional[Location])]:
+        f = open(file_location, encoding='utf-8')
+        lines = f.read().split('\n')[1:]
+        lines = [i.split(';') for i in lines]
+        res = []
+
+        for i in lines:
+            try:
+                full_name = i[0]
+                date_of_birth = i[1]
+                student_id = i[2]
+                contacts = i[3]
+                bio = i[4]
+
+                res.append(
+                    (None, Lesson(full_name, date_of_birth, student_id, contacts, bio)))
+            except IndexError as e:
+                exception_text = f"Строка {lines.index(i) + 2} не добавилась в [res]"
+                print(exception_text)
+                print(e)
+                res.append((exception_text, None))
+            except Exception as e:
+                exception_text = f"Неизвестная ошибка в Student.parse():\n{e}"
+                print(exception_text)
+                res.append((exception_text, None))
+
+        return res
+
+    def __str__(self):
+        return f'Student(full_name = {self.__full_name}, date_of_birth = {self.__date_of_birth}, contacts = {self.__contacts}, bio =  {self.__bio}) '
+
+    def __serialize_to_json(self):
+        return json.dumps({"full_name": self.__full_name,
+                           "date_of_birth": self.__date_of_birth,
+                           "student_id": self.__student_id,
+                           "contacts": self.__contacts,
+                           "bio": self.__bio}, ensure_ascii=False)
+
+    def save(self, file_way="./db/student.json"):
+        with open(file_way, mode="w", encoding='utf-8') as data_file:
+            data_file.write(self.__serialize_to_json())
