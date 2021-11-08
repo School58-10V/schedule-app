c29de260a5d25baa2fecc1544d5ce694501daee2
khrom <khromov05@gmail.com>
2021-11-08 20:59:33 +0300

    пофиксил тесты

--- tests/team_a_parse_save_feature_test.py
+++ tests/team_a_parse_save_feature_test.py
@@ -1,24 +1,22 @@
 from data_model.lesson import Lesson
 from data_model.group import Group
 
-lesson = Lesson(9, 16, 3, 67, 8, 5, "",)
-lesson.save('../db')
+# сохранение Урока в файл .json
+example_lesson = Lesson(9, 16, 3, 67, 8, 5, "")
+print(example_lesson)
+example_lesson.save('../db')
 
-lesson = Lesson()
-lesson = lesson.parse('../data_examples/lesson_test.csv')[0]
-print(lesson)
-lesson.save('../db')
+# чтение из .csv и сохранение Урока в файл .json
+parsed_lesson = Lesson.parse('../data_examples/lesson.csv')[0][1]
+print(parsed_lesson)
+parsed_lesson.save('../db')
 
-lesson = lesson[0][1]
-print(lesson)
-lesson.save('../db')
-print()
-print()
+# сохранение Группы в файл .json
+example_group = Group(6, "A", 7, "ФИЗМАТ")
+print(example_group)
+example_group.save('../db')
 
-group = Group(6, "A", 7, "ФИЗМАТ")
-group = group.parse("../data_examples/group_test.csv")[0]
-print(group)
-print(group)
-group.save('../db')
-print()
-print()
+# чтение из .csv и сохранение Группы в файл .json
+parsed_group = Group.parse("../data_examples/group.csv")[0][1]
+print(parsed_group)
+parsed_group.save('../db')
