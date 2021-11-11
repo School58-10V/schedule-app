b78461e9af229a1a16fcac20580d85249cd34353
DragonMadness_ <supervasya45@gmail.com>
2021-11-11 20:02:25 +0300

    Поправил тест и исправил использования TimeTable на cls

--- data_model/timetable.py
+++ data_model/timetable.py
@@ -28,19 +28,19 @@ class TimeTable:
 
     @classmethod
     def get_all(cls, db_path: str = "./db") -> list[TimeTable]:
-        data = TimeTable.__read_json_db(db_path)
+        data = cls.__read_json_db(db_path)
         output = []
         for elem in data:
-            output.append(TimeTable(elem["time_table_year"], elem["time_table_id"]))
+            output.append(cls(elem["time_table_year"], elem["time_table_id"]))
         return output
 
     @classmethod
-    def get_by_id(cls, elem_id, db_path: str = "./db"):
-        data = TimeTable.__read_json_db(db_path)
+    def get_by_id(cls, elem_id: int, db_path: str = "./db") -> TimeTable:
+        data = cls.__read_json_db(db_path)
         for elem in data:
             if elem["time_table_id"] == elem_id:
-                return TimeTable(elem["time_table_year"], elem["time_table_id"])
-        return ""
+                return cls(elem["time_table_year"], elem["time_table_id"])
+        raise ValueError(f"Объект с id {elem_id} не найден")
 
     def serialize_to_json(self, indent: int = None) -> str:
         return json.dumps(self.__dict__(), ensure_ascii=False, indent=indent)
--- tests/team_X_get_entities_feature.py
+++ tests/team_X_get_entities_feature.py
@@ -2,18 +2,18 @@ from data_model.subject_lesson import Subject
 from data_model.teachers_on_lesson_rows import TeachersOnLessonRows
 from data_model.timetable import TimeTable
 
-teacher1 = TeachersOnLessonRows.get_by_id(5, '../db')
-teachers = TeachersOnLessonRows.get_all('../db')
-print(teacher1)
-print(teachers)
-print()
-print()
+#teacher1 = TeachersOnLessonRows.get_by_id(3, '../db')
+#teachers = TeachersOnLessonRows.get_all('../db')
+#print(teacher1)
+#print(teachers)
+#print()
+#print()
 
-subject1 = Subject.get_by_id(5, '../db')
-subjects = Subject.get_all('../db')
-print(subject1)
-print(subjects)
+#subject1 = Subject.get_by_id(3, '../db')
+#subjects = Subject.get_all('../db')
+#print(subject1)
+#print(subjects)
 
 print(TimeTable.get_all("../db"))
-print(TimeTable.get_by_id(1898, "../db"))
+print(TimeTable.get_by_id(1897, "../db"))
 
