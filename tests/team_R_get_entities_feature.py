dbe583d281a88bb12843e8ba854f9aab2473e394
Olga Bugaenko <olgahaipriv7177@gmail.com>
2021-11-11 19:32:11 +0300

    добавлинны тесты

--- tests/team_R_get_entities_feature.py
+++ tests/team_R_get_entities_feature.py
@@ -1,4 +1,6 @@
 from data_model.teacher import Teacher
+from data_model.student import Student
+from data_model.student_in_group import StudentInGroup
 
 
 res = Teacher.get_all("../data_examples/teacher.json")
@@ -6,4 +8,22 @@ print(res)
 for i in res:
     print(i)
 res_id = Teacher.get_by_id(123, "../data_examples/teacher.json")
+print(res_id)
+
+print("----------")
+
+res = Student.get_all("../data_examples/student.json")
+print(res)
+for i in res:
+    print(i)
+res_id = Student.get_by_id(678, "../data_examples/student.json")
+print(res_id)
+
+print("----------")
+
+res = StudentInGroup.get_all("../data_examples/student_in_group.json")
+print(res)
+for i in res:
+    print(i)
+res_id = StudentInGroup.get_by_id(345, "../data_examples/student_in_group.json")
 print(res_id)
