8433e9f38349a8661467e54ee7d3b3fb87fc3b54
Mitya <you@example.com>
2021-11-11 21:13:21 +0300

    push smthg to get_entities/team_R brunch

--- tests/team_R_get_entities_feature.py
+++ tests/team_R_get_entities_feature.py
@@ -3,27 +3,27 @@ from data_model.student import Student
 from data_model.student_in_group import StudentInGroup
 
 
-res = Teacher.get_all("../data_examples/teacher.json")
+res = Teacher.get_all("../data_examples")
 print(res)
 for i in res:
     print(i)
-res_id = Teacher.get_by_id(123, "../data_examples/teacher.json")
+res_id = Teacher.get_by_id(123, "../data_examples")
 print(res_id)
 
 print("----------")
 
-res = Student.get_all("../data_examples/student.json")
+res = Student.get_all("../data_examples")
 print(res)
 for i in res:
     print(i)
-res_id = Student.get_by_id(678, "../data_examples/student.json")
+res_id = Student.get_by_id(678, "../data_examples")
 print(res_id)
 
 print("----------")
 
-res = StudentInGroup.get_all("../data_examples/student_in_group.json")
+res = StudentInGroup.get_all("../data_examples")
 print(res)
 for i in res:
     print(i)
-res_id = StudentInGroup.get_by_id(345, "../data_examples/student_in_group.json")
+res_id = StudentInGroup.get_by_id(345, "../data_examples")
 print(res_id)
