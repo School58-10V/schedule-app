c1879fbcad3f9fdb7aa519e5f76838a44b71ad47
Olga Bugaenko <olgahaipriv7177@gmail.com>
2021-11-15 18:48:46 +0300

    insert

--- adapters/insert.py
+++ adapters/insert.py
@@ -1,14 +1,25 @@
+# group - 01
+# lesson - 02
+# lesson_row - 03
+# location - 04
+# no_learning_period - 05
+# student - 06
+# student_in_group - 07
+# subject_lesson - 08
+# teacher - 09
+# teachers_on_lesson_rows - 10
+# timetable - 11
 import json
 
 
+
 def insert(self, collection_name: str, document: dict) -> dict:
-    def __dict__(self) -> dict:
-        return {"fio": self.__fio,
-                "teacher_id": self.__teacher_id,
-                "bio": self.__bio,
-                "contacts": self.__contacts,
-                "office_id": self.__office_id,
-                "subject": self.__subject}
+    current_records = self.__read_json_db(collection_name)
+    document["_object_id"] = int(id_class + len(f"./db/{collection_name}.json"))
+    current_records.append(self.__dict__())
+    target_json = self.__class__.serialize_records_to_json(current_records)
+
+
 
     @staticmethod
     def serialize_records_to_json(records: list, indent: int = None) -> str:
