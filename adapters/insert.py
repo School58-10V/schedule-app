c1eb95c63ee6916829998e27b80c3ce05dd58ad6
Olga Bugaenko <olgahaipriv7177@gmail.com>
2021-11-15 18:08:03 +0300

    insert

--- adapters/insert.py
+++ adapters/insert.py
@@ -1,2 +1,32 @@
-def insert(self, collection_name: str, document: dict) -> dict:
+import json
 
+
+def insert(self, collection_name: str, document: dict) -> dict:
+    def __dict__(self) -> dict:
+        return {"fio": self.__fio,
+                "teacher_id": self.__teacher_id,
+                "bio": self.__bio,
+                "contacts": self.__contacts,
+                "office_id": self.__office_id,
+                "subject": self.__subject}
+
+    @staticmethod
+    def serialize_records_to_json(records: list, indent: int = None) -> str:
+        return json.dumps(records, ensure_ascii=False, indent=indent)
+
+    @classmethod
+    def __read_json_db(cls, db_path) -> list:
+        try:
+            with open(f"{db_path}/{cls.__name__}.json",
+                      mode="r", encoding='utf-8') as data_file:
+                record = json.loads(data_file.read())
+                return record
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            return []
+
+    def save(self, output_path: str = './db'):
+        current_records = self.__read_json_db(output_path)
+        current_records.append(self.__dict__())
+        target_json = self.__class__.serialize_records_to_json(current_records)
+        with open(f"{output_path}/{type(self).__name__}.json", mode="w", encoding='utf-8') as data_file:
+            data_file.write(target_json)
