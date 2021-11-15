f143c04de90249cecd09d71d46fe1673062d9594
Мария <kuranova.marie@yandex.ru>
2021-11-15 19:37:29 +0300

    Отредактировала еще класс

--- data_model/group.py
+++ data_model/group.py
@@ -1,10 +1,10 @@
 from __future__ import annotations  # нужно чтобы parse мог быть типизирован
 
-import json
+from data_model.abstract_model import AbstractModel
 from typing import Optional, List
 
 
-class Group:
+class Group(AbstractModel):
     """
         Класс группы.
     """
@@ -12,7 +12,7 @@ class Group:
     def __init__(
             self, teacher_id: int, class_letter: str, grade: int,
             profile_name: str, group_id: Optional[int] = None
-    ):
+            ):
         self.__teacher_id = teacher_id
         self.__class_letter = class_letter
         self.__grade = grade
@@ -34,39 +34,16 @@ class Group:
     def get_id(self) -> Optional[int]:
         return self.__group_id
 
-    def save(self, output_path: str = './db'):
-        current_records = self.__read_json_db(output_path)
-        current_records.append(self.__dict__())
-        target_json = self.__serialize_records_to_json(current_records)
-        with open(f"{output_path}/{type(self).__name__}.json", mode="w", encoding='utf-8') as data_file:
-            data_file.write(target_json)
-
-    def serialize_to_json(self, indent: Optional[int] = None) -> str:
-        return json.dumps(self.__dict__(), ensure_ascii=False, indent=indent)
-
-    @classmethod
-    def __read_json_db(cls, db_path) -> list:
-        try:
-            with open(f"{db_path}/{cls.__name__}.json", mode="r", encoding='utf-8') as data_file:
-                record = json.loads(data_file.read())
-                return record
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
-            return []
-
     @classmethod
     def get_all(cls, db_path: str = "./db") -> List[Group]:
-        return [cls(**i) for i in cls.__read_json_db(db_path)]
+        return [cls(**i) for i in cls._read_json_db(db_path)]
 
     @classmethod
     def get_by_id(cls, element_id: int, db_path: str = "./db") -> Group:
-        for i in cls.__read_json_db(db_path):
+        for i in cls._read_json_db(db_path):
             if i['group_id'] == element_id:
                 return cls(**i)
         raise ValueError(f"Объект с id {element_id} не найден")
-
-    @staticmethod
-    def __serialize_records_to_json(records: list, indent: int = None):
-        return json.dumps(records, ensure_ascii=False, indent=indent)
 
     @staticmethod
     def parse(file_location: str) -> List[(Optional[str], Optional[Group])]:
@@ -104,3 +81,9 @@ class Group:
                 "grade": self.__grade,
                 "profile_name": self.__profile_name,
                 "group_id": self.__group_id}
+
+    def get_main_id(self):
+        return self.__group_id
+
+    def _set_main_id(self, elem_id: Optional[int] = None):
+        self.__group_id = elem_id
--- tests/team_XA_abstarct_class_feature.py
+++ tests/team_XA_abstarct_class_feature.py
@@ -1,7 +1,10 @@
 from data_model.subject_lesson import Subject
 from data_model.teachers_on_lesson_rows import TeachersOnLessonRows
+from data_model.group import Group
 # from data_model.timetable import TimeTable
 #
+teacher = TeachersOnLessonRows(2, 3, teacher_on_lesson_row_id=5)
+teacher.save('../db')
 teacher1 = TeachersOnLessonRows.get_by_id(5, '../db')
 teachers = TeachersOnLessonRows.get_all('../db')
 print(teacher1)
@@ -13,7 +16,8 @@ print(teacher1)
 print()
 print()
 
-
+subject = Subject(subject_id=5)
+subject.save('../db')
 subject1 = Subject.get_by_id(5, '../db')
 subjects = Subject.get_all('../db')
 print(subject1)
@@ -22,6 +26,17 @@ subject1.delete('../db')
 subjects = Subject.get_all('../db')
 print(subjects)
 print()
+
+group1 = Group(1, '', 1, 'k', 10)
+group1.save('../db')
+group2 = Group.get_by_id(10, '../db')
+groups = Group.get_all('../db')
+print(group2)
+print(groups)
+group2.delete('../db')
+groups = Group.get_all('../db')
+print(groups)
+print(group2)
 
 # print(TimeTable.get_all("../db"))
 # print(TimeTable.get_by_id(1898, "../db"))
