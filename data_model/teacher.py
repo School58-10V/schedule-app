4f9aff8d21d3dbf7d3c60192e2142494bbf7eaa0
DragonMadness_ <supervasya45@gmail.com>
2021-11-15 20:39:44 +0300

    Подключил Teacher.py к абстракту

--- data_model/teacher.py
+++ data_model/teacher.py
@@ -1,6 +1,5 @@
 from __future__ import annotations  # нужно чтобы parse мог быть типизирован
-import json
-
+from data_model.abstract_model import AbstractModel
 from typing import Optional, List
 
 
@@ -55,8 +54,7 @@ class Teacher:
                     bio = i[3]
                     contacts = i[4]
 
-                    res.append(
-                        (None, Teacher(fio, subject, office_id, bio, contacts)))
+                    res.append((None, Teacher(fio, subject, office_id, bio, contacts)))
                 except IndexError as e:
                     exception_text = f"Строка {lines.index(i) + 1} не добавилась в [res]"
                     print(exception_text)
@@ -66,7 +64,6 @@ class Teacher:
                     exception_text = f"Неизвестная ошибка в Teacher.parse():\n{e}"
                     print(exception_text)
                     res.append((exception_text, None))
-
         return res
 
     def __dict__(self) -> dict:
@@ -80,27 +77,3 @@ class Teacher:
     def __str__(self):
         return f'Teacher(fio = {self.__fio}, subject = {self.__subject}, bio = {self.__bio}, ' \
                f'contacts =  {self.__contacts}) '
-
-    def serialize_to_json(self, indent: Optional[int] = None):
-        return json.dumps(self.__dict__(), ensure_ascii=False, indent=indent)
-
-    @staticmethod
-    def serialize_records_to_json(records: list, indent: int = None) -> str:
-        return json.dumps(records, ensure_ascii=False, indent=indent)
-
-    @classmethod
-    def __read_json_db(cls, db_path) -> list:
-        try:
-            with open(f"{db_path}/{cls.__name__}.json",
-                      mode="r", encoding='utf-8') as data_file:
-                record = json.loads(data_file.read())
-                return record
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
-            return []
-
-    def save(self, output_path: str = './db'):
-        current_records = self.__read_json_db(output_path)
-        current_records.append(self.__dict__())
-        target_json = self.__class__.serialize_records_to_json(current_records)
-        with open(f"{output_path}/{type(self).__name__}.json", mode="w", encoding='utf-8') as data_file:
-            data_file.write(target_json)
