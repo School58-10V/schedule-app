4bc9ed22942cc2cadd2beb11d9a701a3dd80de0c
марина( <rinamgame@gmail.com>
2021-11-22 19:41:29 +0300

    сделала тесты гетеров

--- tests/test_RK_location.py
+++ tests/test_RK_location.py
@@ -2,4 +2,13 @@ from data_model.location import Location
 from adapters.file_source import FileSource
 
 a = FileSource()
-a.insert('Location', {"location_type": "кабинет", "name": "213", "link": "None", "comment": "Математика"})
+# a.insert('Location', {"location_type": "кабинет", "name": "213", "link": "None", "comment": "Математика"})
+
+# комментарий, так как я его уже себе добавила и теперь у меня есть его айди. На вашем устройстве придется
+# его перезаписать и проверить его айди.
+
+print(a.get_by_id('Location', 1041637588222989)) # у меня работает
+# a.insert('Location', {"location_type": "кабинет", "name": "228", "link": "None", "comment": "Русский Язык"})
+
+# та же ситуация, что и с прошлым инсертом
+print(a.get_all('Location'))
