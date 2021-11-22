9e01348038c5372c099a8f56096858daa19d21df
khrom <khromov05@gmail.com>
2021-11-22 18:59:18 +0300

    сделал комменты поприятнее, и добавил описания параметров/возвращаемых значений в AbstractModel

--- data_model/abstract_model.py
+++ data_model/abstract_model.py
@@ -7,11 +7,16 @@ from typing import Optional, List
 
 class AbstractModel(ABC):
     """
-        Абстрактный класс модели
+        Абстрактный класс сущности
     """
 
-    # Добавляет данные текущего объекта в конец сохранения используя __dict__() этого класса
-    def save(self, output_path: str = './db'):
+    def save(self, output_path: str = './db') -> AbstractModel:
+        """
+        Добавляет данные текущего объекта в конец файла сохранения используя __dict__() этого класса
+
+        :param output_path: путь до папки с .json файлами
+        :return: объект который мы сохранили
+        """
         current_records = self._read_json_db(output_path)  # Читает старую версию файла и записывает список словарей
         current_records.append(self.__dict__())  # Добавляет настоящий объект
         target_json = AbstractModel._serialize_records_to_json(current_records)  # Готовит данные для формата JSON
@@ -19,8 +24,13 @@ class AbstractModel(ABC):
             data_file.write(target_json)
         return self  # Возвращает объект который был сохранён
 
-    # Удаляет сохранения и id-связи текущего объекта
-    def delete(self, db_path: str = './db'):
+    def delete(self, db_path: str = './db') -> AbstractModel:
+        """
+        Удаляет сохранения и id-связи текущего объекта
+
+        :param db_path: путь до папки с .json файлами
+        :return: объект который мы удалили
+        """
         if self._object_id is None:  # Проверяет валидность данного объекта
             return self
         data = self._read_json_db(db_path)  # Читает файл сохранения и возвращает список словарей
@@ -35,26 +45,48 @@ class AbstractModel(ABC):
         self._set_main_id(None)  # В любом случае уничтожает все id-связи данного объекта
         return self  # Возвращает удалённый объект с self._object_id = None
 
-    # Превращает один обьект в JSON-строку
     def serialize_to_json(self, indent: Optional[int] = None) -> str:
+        """
+        Превращает данный объект класса в JSON-строку
+
+        :param indent: табуляция для полученной строки json-а
+        :return: json-строку
+        """
         return json.dumps(self.__dict__(), ensure_ascii=False, indent=indent)
 
     @classmethod
-    # Возвращает все данные из сохранений в формате объектов соответствующих классов
     def get_all(cls, db_path: str = "./db") -> List[AbstractModel]:
+        """
+        Возвращает все данные из сохранений в формате объектов соответствующих классов
+
+        :param db_path: путь до папки с .json файлами
+        :return: Список всех объектов этого класса
+        """
         return [cls(**i) for i in cls._read_json_db(db_path)]
 
     @classmethod
-    # Возвращает запрощенный по element_id объект класса по данным из сохранений
     def get_by_id(cls, element_id: int, db_path: str = "./db") -> AbstractModel:
-        for i in cls._read_json_db(db_path):  # Проходит по списку словарей в файле сохранения
+        """
+        Возвращает запрощенный по element_id объект класса по данным из сохранений
+
+        :param element_id: айдишник объекта
+        :param db_path: путь до папки с .json файлами
+        :return: Объект этого класса с таким идшником
+        """
+        # Проходит по списку словарей в файле сохранения
+        for i in cls._read_json_db(db_path):
             if i['object_id'] == element_id:
                 return cls(**i)
         raise ValueError(f"Объект с id {element_id} не найден")
 
     @classmethod
-    # Читает файл сохранения текущего объекта и возращает пустой список при ошибке
     def _read_json_db(cls, db_path: str) -> List[dict]:
+        """
+        Читает файл сохранения текущего объекта и возращает пустой список при ошибке
+
+        :param db_path: путь до папки с .json файлами
+        :return:
+        """
         try:
             with open(f"{db_path}/{cls.__name__}.json",
                       mode="r", encoding='utf-8') as data_file:
@@ -64,8 +96,14 @@ class AbstractModel(ABC):
             return []
 
     @staticmethod
-    # Подготавливает входные данные к записи в JSON и возвращает JSON-строку
     def _serialize_records_to_json(records: List[dict], indent: Optional[int] = None) -> str:
+        """
+        Подготавливает входные данные к записи в JSON
+
+        :param records: список словарей каких-то объектов
+        :param indent: табуляция для полученной строки json-а
+        :return: json-строку
+        """
         return json.dumps(records, ensure_ascii=False, indent=indent)
 
     @abstractmethod
@@ -76,11 +114,20 @@ class AbstractModel(ABC):
     def __dict__(self):
         pass
 
-    # Возвращает айди текущего объекта
     def get_main_id(self):
+        """
+        Возвращает айди текущего объекта
+
+        :return: айдишнк текущего объекта
+        """
         return self._object_id
 
-    # Меняет айди текущего объекта на предоставленное значение
-    def _set_main_id(self, elem_id: Optional[int] = None):
+    def _set_main_id(self, elem_id: Optional[int] = None) -> AbstractModel:
+        """
+        Меняет айди текущего объекта на предоставленное значение
+
+        :param elem_id: новый айдишник
+        :return: текущий объект
+        """
         self._object_id = elem_id
         return self
