d939e96e15d0b2eb491b52ae7ab5b24f7e8ea6bb
марина( <rinamgame@gmail.com>
2021-11-25 19:55:34 +0300

    оно не работает, т.к. в абстракт моделе не назначаются айдишники)

--- tests/test_RK_location.py
+++ tests/test_RK_location.py
@@ -1,5 +1,14 @@
 from data_model.location import Location
+from data_model.abstract_model import AbstractModel
 from adapters.file_source import FileSource
 
-location =Location.parse(../data_examples/)
+
+file_source = FileSource()
+# location = Location.parse("../data_examples/location.csv")
+# print(*location)
+# location_list = []
+# for element in location:
+#     element[1].save('../db')
+# file_source.insert('Location', {"location_type": "кабинет", "name": "258", "link": "None", "comment": "Физика"})
+# file_source.insert('Location', {"location_type": "кабинет", "name": "290", "link": "None", "comment": "Астрономия"})
 
