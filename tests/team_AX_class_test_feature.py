df284914db213175b22392d7b0d91930af9624d8
khrom <khromov05@gmail.com>
2021-11-29 19:36:24 +0300

    пофиксил тесты, сделал тесты для учителя, сделал .csv файл с тестами для учителя.

--- tests/team_AX_class_test_feature.py
+++ tests/team_AX_class_test_feature.py
@@ -1,11 +1,13 @@
 import random
 
+from data_model.teacher import Teacher
 from data_model.teachers_on_lesson_rows import TeachersOnLessonRows
 from adapters.file_source import FileSource
 from data_model.timetable import TimeTable
 from data_model.subject_lesson import Subject
 
-fS = FileSource('../db')
+db_location = '../db'
+fS = FileSource(db_location)
 
 
 def test_function(TestingClass, example_1: dict, example_2: dict,
@@ -45,12 +47,14 @@ def test_function(TestingClass, example_1: dict, example_2: dict,
     print(f'fs.insert() еще два объекта: \n{object_2}\n{object_3}\n')
 
     # Удалим первый объект
-    from_csv_object.delete('../db')
-    print(f'Удалили объект который был из .csv (id=None теперь):\n{from_csv_object}\n')
+    deleted_object = fS.delete(TestingClass.__name__, from_csv_object.get_main_id())
+    print(f'Удалили объект который был из .csv, его ID было:\n{deleted_object}\n')
     print('Все объекты в db/.json (class):', *TestingClass.get_all('../db'), sep='\n', end='\n\n')
-    object_2 = fS.update(object_id=object_2['object_id'], document=example_1_update, collection_name=TestingClass.__name__)
+    object_2 = fS.update(object_id=object_2['object_id'], document=example_1_update,
+                         collection_name=TestingClass.__name__)
 
-    object_3 = fS.update(object_id=object_3['object_id'], document=example_2_update, collection_name=TestingClass.__name__)
+    object_3 = fS.update(object_id=object_3['object_id'], document=example_2_update,
+                         collection_name=TestingClass.__name__)
 
     print(f'Изменили 2 объекта:\n{object_2}\n{object_3}\n')
     print('Все объекты в db/.json (class):', *TestingClass.get_all('../db'), sep='\n', end='\n\n')
@@ -66,6 +70,9 @@ def test_function(TestingClass, example_1: dict, example_2: dict,
 
     fS.insert(TestingClass.__name__, from_csv_object.__dict__())
     print("Вернули первый объект, все файлы (adapter):", *fS.get_all(TestingClass.__name__), sep='\n')
+    print("Убираем все объекты из файла...")
+    for i in fS.get_all(TestingClass.__name__):
+        fS.delete(TestingClass.__name__, i['object_id'])
 
     print(f'------ КОНЕЦ ТЕСТА КЛАССА {TestingClass.__name__} ------\n\n')
 
@@ -79,79 +86,28 @@ test_function(TeachersOnLessonRows, example_1=example_1, example_2=example_2,
               csv_location='../data_examples/teachers_on_lesson_rows_test.csv',
               )
 
-# print('\n\nНовый класс!!!!!!!!!!!')
-# timetables = TimeTable.parse('../data_examples/timetable_test.csv')
-# print(f'Список всех объектов из csv', *[i[1] for i in timetables])
-# # Берем один объект из списка и сохраняем его
-# timetable = timetables[0][-1]
-# timetable = TimeTable(**fS.insert(TimeTable.__name__, timetable.__dict__()))
-# print(f'Первый объект: {timetable}\n')
-# print(f'Все объекты:', *fS.get_all(TimeTable.__name__))
-# # Создаем еще два объекта
-# timetable1 = TimeTable(object_id=5, time_table_year=2000)
-# timetable1.save('../db')
-# timetable2 = TimeTable(time_table_year=1, object_id=6)
-# timetable2.save('../db')
-# print(f'Еще два объекта: {timetable1}, {timetable2}\n')
-# # Удалим первый объект
-# timetable.delete('../db')
-# print(f'Удалили объект 1: {timetable}\n')
-# print('Все объекты:', *TimeTable.get_all('../db'))
-# timetable1 = TimeTable(time_table_year=2011, object_id=5)
-# fS.update(object_id=5, document=timetable1.__dict__(),
-#           collection_name=TimeTable.__name__)
-# timetable2 = TimeTable(time_table_year=112121212121121211, object_id=6)
-# fS.update(object_id=6, document=timetable2.__dict__(),
-#           collection_name=TimeTable.__name__)
-#
-# print(f'Изменили 2 объекта: {timetable1}; {timetable2} \n')
-# print('Все объекты через класс:', *TimeTable.get_all('../db'))
-# print('Список всех через адаптер: ', fS.get_all(TimeTable.__name__), '\n')
-# print('Ищем через адаптер: ', fS.get_by_id(collection_name=TimeTable.__name__,
-#                                            object_id=timetable2.get_main_id()))
-#
-# print("Ищем через адаптер рандомный объект: ", fS.get_by_id(TimeTable.__name__,
-#                                                             2), end='\n\n')
-# print("Опять все файлы?", *fS.get_all(TimeTable.__name__))
-# fS.insert(TimeTable.__name__,
-#           TimeTable(time_table_year=2005, object_id=None).__dict__())
-#
-# print("Опять все файлы?", *fS.get_all(TimeTable.__name__))
-#
-# subject_lessons = Subject.parse('../data_examples/timetable_test.csv')
-# print(f'Список всех объектов из csv', *[i[1] for i in subject_lessons])
-# # Берем один объект из списка и сохраняем его
-# subject_lesson = subject_lessons[0][-1]
-# subject_lesson = Subject(**fS.insert(Subject.__name__, subject_lesson.__dict__()))
-# print(f'Первый объект: {subject_lesson}\n')
-# print(f'Все объекты:', *fS.get_all(Subject.__name__))
-# # Создаем еще два объекта
-# subject_lesson1 = Subject(object_id=5, subject_name="Algebra")
-# subject_lesson1.save('../db')
-# subject_lesson2 = Subject(subject_name="Physics", object_id=6)
-# subject_lesson2.save('../db')
-# print(f'Еще два объекта: {subject_lesson1}, {subject_lesson2}\n')
-# # Удалим первый объект
-# subject_lesson.delete('../db')
-# print(f'Удалили объект 1: {subject_lesson}\n')
-# print('Все объекты:', *Subject.get_all('../db'))
-# subject_lesson1 = Subject(subject_name="PE", object_id=5)
-# fS.update(object_id=5, document=subject_lesson1.__dict__(),
-#           collection_name=Subject.__name__)
-# subject_lesson2 = Subject(subject_name="Eng", object_id=6)
-# fS.update(object_id=6, document=subject_lesson2.__dict__(),
-#           collection_name=Subject.__name__)
-#
-# print(f'Изменили 2 объекта: {subject_lesson1}; {subject_lesson2} \n')
-# print('Все объекты через класс:', *Subject.get_all('../db'))
-# print('Список всех через адаптер: ', fS.get_all(Subject.__name__), '\n')
-# print('Ищем через адаптер: ', fS.get_by_id(collection_name=Subject.__name__,
-#                                            object_id=subject_lesson2.get_main_id()))
-#
-# print("Ищем через адаптер рандомный объект: ", fS.get_by_id(Subject.__name__,
-#                                                             2), end='\n\n')
-# print("Опять все файлы?", *fS.get_all(Subject.__name__))
-# fS.insert(Subject.__name__,
-#           Subject(subject_name="Geometry", object_id=None).__dict__())
-#
-# print("Опять все файлы?", *fS.get_all(Subject.__name__))
+example_1 = {"time_table_year": 2000}
+example_2 = {"time_table_year": 1}
+example_1_update = {"time_table_year": 2021}
+example_2_update = {"time_table_year": 2}
+test_function(TimeTable, example_1=example_1, example_2=example_2, example_1_update=example_1_update,
+              example_2_update=example_2_update, csv_location='../data_examples/timetable_test.csv'
+              )
+
+example_1 = {'subject_name': 'Алгебра'}
+example_2 = {'subject_name': 'Теория вероятности'}
+example_1_update = {'subject_name': 'Algebra'}
+example_2_update = {'subject_name': 'Theory of chance'}
+test_function(Subject, example_1=example_1, example_2=example_2, example_1_update=example_1_update,
+              example_2_update=example_2_update, csv_location='../data_examples/subject_test.csv'
+              )
+
+example_1 = {'fio': 'Афанасьев Александр Николаевич', 'bio': 'учитель инфы', 'contacts': '*контакты*', 'office_id': 12,
+             'subject': 'Информатика'}
+example_2 = {'fio': 'Капустин Андрей Андреевич', 'bio': 'учитель инфы', 'contacts': '*контакты*', 'office_id': 12,
+             'subject': 'Информатика'}
+example_1_update = {'office_id': 13}
+example_2_update = {'office_id': 13}
+test_function(Teacher, example_1=example_1, example_2=example_2, example_1_update=example_1_update,
+              example_2_update=example_2_update, csv_location='../data_examples/teacher.csv'
+              )
