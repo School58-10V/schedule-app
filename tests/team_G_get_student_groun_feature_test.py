15373a332614b372576f0e7c77a9ebdcfb539d02
Мария <kuranova.marie@yandex.ru>
2021-12-06 21:04:08 +0300

    тест

--- tests/team_G_get_student_groun_feature_test.py
+++ tests/team_G_get_student_groun_feature_test.py
@@ -2,6 +2,5 @@ from data_model.student import Student
 from adapters.file_source import FileSource
 
 data = FileSource('../db')
-student = Student(db_source=data, full_name='', date_of_birth=6)
-student.save()
+student = Student.get_by_id(1061638802407770, data)
 print(student.get_all_student_group())
