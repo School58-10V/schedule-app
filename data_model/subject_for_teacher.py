b3c4811c45a608f35e98aced500a99edf6b5d939
Mitya <you@example.com>
2021-12-06 21:10:57 +0300

    добавил методы dict и str, метод parce

--- data_model/subject_for_teacher.py
+++ data_model/subject_for_teacher.py
@@ -16,14 +16,14 @@ class SubjectForTeacher(AbstractModel):
         """
     :param teacher_id - Идентификационный номер Teacher
     :param subject_id - Идентификационный номер Subject
-    :param object_id - Идентификационный номер Student_for_teacher(опционально)
+    :param object_id - Идентификационный номер SubjectForTeacher(опционально)
 
     """
 
         super().__init__(db_source)
         self.__teacher_id = teacher_id
         self.__subject_id = subject_id
-        self.__object_id = object_id
+        self._object_id = object_id
 
     def get_teacher_id(self) -> int:
         return self.__teacher_id
@@ -31,3 +31,35 @@ class SubjectForTeacher(AbstractModel):
     def get_subject_id(self) -> int:
         return self.__subject_id
 
+    def __str__(self):
+        return f'SubjectForTeacher(teacher_id: {self.__teacher_id},' \
+               f' subject_id: {self.__subject_id},' \
+               f' object_id: {self._object_id})'
+
+    def __dict__(self):
+        return {"teacher_id": self.__teacher_id,
+                "subject_id": self.__subject_id,
+                "object_id": self._object_id}
+
+    @staticmethod
+    def parse(file_location: str) -> List[(Optional[str], Optional[SubjectForTeacher])]:
+        file = open(file_location, 'r', encoding='utf-8')
+        lines = file.read().split('\n')[1:]
+        file.close()
+        res = []
+        for i in lines:
+            j = i.split(';')
+            try:
+                teacher_id = int(j[0])
+                subject_id = int(j[1])
+
+                res.append(ParsedData(None, SubjectForTeacher(teacher_id=teacher_id, subject_id=subject_id)))  # надо добавить db_source
+            except (IndexError, ValueError) as error:
+                exception_text = f"Запись {lines.index(i) + 1} строка {lines.index(i) + 2} " \
+                                 f"не добавилась в [res].\nОшибка: {error}"
+                res.append(ParsedData(exception_text, None))
+            except Exception as error:
+                exception_text = f"Неизвестная ошибка в SubjectForTeacher.parse():\n{error}"
+                print(exception_text + '\n')
+                res.append(ParsedData(exception_text, None))
+        return res
