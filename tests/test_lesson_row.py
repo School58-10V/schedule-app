9c14f2fb4a4480f80c313493abffc7fe895968b9
марина( <rinamgame@gmail.com>
2021-12-09 18:33:58 +0300

    пофиксила тесты на lesson_row

--- tests/test_lesson_row.py
+++ tests/test_lesson_row.py
@@ -1,10 +1,10 @@
 from data_model.lesson_row import LessonRow
 from tests.team_AX_class_test_feature import test_function
 
-example_1 = {'teacher_id': 10, 'class_letter': 'Б', 'grade': 10, 'profile_name': 'Математика'}
-example_2 = {'teacher_id': 10, 'class_letter': 'А', 'grade': 10, 'profile_name': 'Математика'}
-example_1_update = {'class_letter': 'В'}
-example_2_update = {'profile_name': 'Русский Язык'}
+example_1 = {'count_studying_hours': 12, 'group_id': 101, 'subject_id': 102, 'room_id': 416, 'start_time': 1230, 'end_time': 1315, 'timetable_id': 1}
+example_2 = {'count_studying_hours': 24, 'group_id': 102, 'subject_id': 105, 'room_id': 413, 'start_time': 1030, 'end_time': 1115, 'timetable_id': 1}
+example_1_update = {'count_studying_hours': 36}
+example_2_update = {'subject_id': 100238}
 test_function(LessonRow, example_1=example_1, example_2=example_2, example_1_update=example_1_update,
-               example_2_update=example_2_update, csv_location='../data_examples/group.csv'
-               )
+              example_2_update=example_2_update, csv_location='../data_examples/lesson_row.csv'
+              )
