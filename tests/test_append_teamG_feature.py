e6a88db35969f38972325f1371fce13c84793e03
Мария <kuranova.marie@yandex.ru>
2021-12-20 20:37:05 +0300

    Тест

---
+++ tests/test_append_teamG_feature.py
@@ -0,0 +1,15 @@
+from data_model.group import Group
+from data_model.student import Student
+from adapters.file_source import FileSource
+db_source = FileSource('../db')
+student = Student(db_source=db_source, date_of_birth='', full_name='Фио')
+student.save()
+group = Group(db_source=db_source, teacher_id=2, class_letter='', grade=3, profile_name=', ')
+group.save()
+print('Было:', *group.get_all_students())
+print('Было:',*student.get_all_groups())
+student.append(group)
+group.append(student)
+print("Стало:", *group.get_all_students())
+print("Стало,", *student.get_all_groups())
+
