27bf5acf9c99111af6e06de26e849b77a6635d08
Мария <kuranova.marie@yandex.ru>
2021-12-20 20:23:48 +0300

    Метод append для класса student

--- data_model/student.py
+++ data_model/student.py
@@ -89,3 +89,12 @@ class Student(AbstractModel):
            :return: список объектов Group
         """
         return StudentsForGroups.get_group_by_student_id(self.get_main_id(), self.get_db_source())
+
+    def append(self, group: Group) -> Student:
+        """
+            Сохраняем новую группу для этого студента, используя класс
+        :param group: объект класса Group, который мы хотим добавить этому студенту StudentsForGroups
+        :return: себя
+        """
+        StudentsForGroups(self._db_source, group_id=group.get_main_id(), student_id=self.get_main_id()).save()
+        return self
