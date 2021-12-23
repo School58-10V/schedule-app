0c5e6710fcc7b5ed02024b73a105c7e221db0d2e
DragonMadness_ <supervasya45@gmail.com>
2021-12-23 21:23:54 +0300

    Починены тесты

--- tests/team_Z_append_teacher_into_lesson_row.py
+++ tests/team_Z_append_teacher_into_lesson_row.py
@@ -5,7 +5,7 @@ from data_model.teacher import Teacher
 db = FileSource(db_path='../db')
 
 example_lesson_row = LessonRow(db_source=db, count_studying_hours=10, end_time=10, group_id=10,
-                               room_id=10, start_time=10, subject_id=10, timetable_id=10)
+                               room_id=10, start_time=10, subject_id=10, timetable_id=10).save()
 
 example_teacher = Teacher(fio='а', bio='а', office_id=10, db_source=db).save()
 
