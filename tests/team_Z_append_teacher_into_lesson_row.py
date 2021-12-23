47f3a17e83daf220b48bbffd29f39fc020089d21
DragonMadness_ <supervasya45@gmail.com>
2021-12-23 21:26:47 +0300

    Починены тесты

--- tests/team_Z_append_teacher_into_lesson_row.py
+++ tests/team_Z_append_teacher_into_lesson_row.py
@@ -1,6 +1,7 @@
 from adapters.file_source import FileSource
 from data_model.lesson_row import LessonRow
 from data_model.teacher import Teacher
+from data_model.teachers_for_lesson_rows import TeachersForLessonRows as TFL
 
 db = FileSource(db_path='../db')
 
@@ -13,10 +14,12 @@ print(f"Добавляю связь с {example_teacher.__dict__()}")
 
 example_lesson_row.append_teacher(example_teacher)
 
-print(f'Файл со связями: {db.get_all("SubjectsAndTeachers")}')
+example_lesson_row.append_teacher(example_teacher)
+
+print(f'Файл со связями: {TFL.get_all(db_source=db)}')
 
 print(f"Удаляю связь с {example_teacher.__dict__()}")
 
 example_lesson_row.remove_teacher(example_teacher)
 
-print(f'Файл со связями: {db.get_all("SubjectsAndTeachers")}')
+print(f'Файл со связями: {TFL.get_all(db_source=db)}')
