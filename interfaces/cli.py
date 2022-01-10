572bed75f77be232c81a6511f91215c081c85f5d
Мария <kuranova.marie@yandex.ru>
2022-01-10 21:28:09 +0300

    Правки

--- interfaces/cli.py
+++ interfaces/cli.py
@@ -1,6 +1,10 @@
 import json
+from typing import TYPE_CHECKING
 
+from data_model.group import Group
 
+if TYPE_CHECKING:
+    from adapters.file_source import FileSource
 # Уровни допуска-
 # 0- учащийся
 # 1- преподаватель
@@ -8,7 +12,7 @@ import json
 
 
 class CLI:
-    def __init__(self):
+    def __init__(self, db_source: FileSource):
         self.data_of_panel = ["    Доступные действия:\n"
                               "информация       завершить сессию\n"
                               "          \     /\n"
@@ -16,6 +20,8 @@ class CLI:
         login = input()  # Собираем с пользователя его данные, чтобы узнать, кто он
         password = input()
         entrance = False
+        self.__db_source = db_source
+
         while entrance is False:
             with open("interfaces/logins_and_passwords.json") as file:
                 data = file.read()
@@ -30,6 +36,11 @@ class CLI:
         if self.__status == 0:
             print('\n'.join([f'Группа {i.get_letter()}' for i in self.__user.get_all_groups()]))
 
+    # Не успела доделать
+    # def __parse_new_group(self, file_location):
+    #     if self.__status == 0:
+    #         groups = Group.parse(file_location, self.__db_source)
+
     def __get_all_subjects(self):
         if self.__status == 1:
             print('\n'.join([f'Предмет {i.get_subject_name()}' for i in self.__user.get_subjects()]))
@@ -41,10 +52,11 @@ class CLI:
     #         self.__user.
 
     def show_menu(self, num_of_panel):
-        print(self.data[num_of_panel])
+        print(self.data[num_of_panel])  # Что это?
         answer = False
         # Необходимо преорбазовать отбор ввода в отдельную функцию(принимаем на ввод корректные значения,
         # ругаем пользователя, пока он не введет одно из них)
+        ans = ''
         while answer is False:
             ans = input()
             if ans in ["1", "2"]:
