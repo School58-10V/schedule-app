fc3edc5984d2ee87c30a4c56f38ca72adb90cc82
Eneev <ivan.eneev@tandex.ru>
2022-01-10 20:31:17 +0300

    Рандомная дичь, которая не работает.

--- interfaces/cli.py
+++ interfaces/cli.py
@@ -2,3 +2,10 @@ class CLI:
     def __init__(self):
         login = input()
         password = input()
+        entrance = False
+        while entrance is False:
+            with open("interfaces/logins_and_passwords.json") as data:
+                for i in data:
+                    if data[i]["login"] == login and login[i]["password"] == password:
+                        entrance = True
+                        status = data[i]["status"]
