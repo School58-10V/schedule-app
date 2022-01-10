ef359bcc911da73408d903c3a61862da80f431b1
Mitya <you@example.com>
2022-01-10 21:04:58 +0300

    добавил cli

--- interfaces/cli.py
+++ interfaces/cli.py
@@ -1,16 +1,22 @@
-# ------------------------
-# [1] show_timetable
-# ------------------------
-# [2] show_teachers
-# ------------------------
-menu = [{}, {}]
+menu = '''------------------------
+[1] show_timetable
+------------------------
+[2] show_teachers
+------------------------
+[3] show_classes
+------------------------
+[4] get_user_info
+------------------------'''
 
-class Cli:
+
+class CLI:
     def __init__(self, user_status: str, class_of_user: str, name: str, surname: str):
         _user_status = user_status
         _class_of_user = class_of_user
         _name = name
         _surname = surname
+        identifier = []
+        __logged_in = False
 
     def __show_timetable(self):
         pass
@@ -18,13 +24,13 @@ class Cli:
     def __show_classes(self):
         pass
 
-    @classmethod
-    def __log_in(cls) -> str:
+    def get_user_info(self):
         pass
 
     def __show_teachers(self):
         pass
 
-    @classmethod
-    def __show_menu(cls):
-        pass
+    def show_menu(cls):
+
+
+
