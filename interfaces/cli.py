275c02e16533e516184c0bcc93fe9ea0b4dbc688
Мария <kuranova.marie@yandex.ru>
2022-01-10 20:47:39 +0300

    Правка

--- interfaces/cli.py
+++ interfaces/cli.py
@@ -3,12 +3,13 @@ import json
 
 class CLI:
     def __init__(self):
-        login = input()
+        login = input()  # Собираем с пользователя его данные, чтобы узнать, кто он
         password = input()
         entrance = False
         while entrance is False:
-            with open("interfaces/logins_and_passwords.json") as data:
-                read_data = json.load(data)
+            with open("interfaces/logins_and_passwords.json") as file:
+                data = file.read()
+                read_data = json.loads(data)
                 for i in read_data:
                     if i["login"] == login and i["password"] == password:
                         entrance = True
