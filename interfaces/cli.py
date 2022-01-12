9a94cd9f290ddcc5f2aed7a6f885a684fb40fe03
марина( <rinamgame@gmail.com>
2022-01-12 22:54:11 +0300

    косметика

--- interfaces/cli.py
+++ interfaces/cli.py
@@ -10,9 +10,8 @@ class CLI:
                       3: "3. показать всех учеников школы", 4: "4. показать расписание"}
 
     def show_menu(self):
-        print('Меню:'
-              'Список команд.')
-        print([self.tasks[i] for i in range(1, len(self.tasks) + 1)], sep="\n")
+        print('Меню:', 'Список команд.', sep='\n')
+        print(*[self.tasks[i] for i in range(1, len(self.tasks) + 1)], sep="\n", end='\n')
         print("Выберите номер нужной команды из списка или введите 0 для окончания работы с интерфейсом.")
         number = int(input())
         if number:
@@ -28,20 +27,29 @@ class CLI:
             return
 
     def __show_all_teachers(self):
-        print(fs.get_all('Teacher'))
+        all_teachers = fs.get_all('Teacher')
+        for teacher in all_teachers:
+            del teacher['object_id']
+        print(*[teacher for teacher in all_teachers], sep='\n')
         self.show_menu()
 
     def __show_all_subjects(self):
-        print(fs.get_all('Subject'))
+        all_subject = fs.get_all('Subject')
+        for subject in all_subject:
+            del subject['object_id']
+        print(*[subject for subject in all_subject], sep='\n')
         self.show_menu()
 
     def __show_all_students(self):
-        print(fs.get_all('Student'))
+        all_student = fs.get_all('Student')
+        for student in all_student:
+            del student['object_id']
+        print(*[student for student in all_student], sep='\n')
         self.show_menu()
 
     def __show_timetable(self):
         subjects = fs.get_all('LessonRow')
-        column_list = ['count_studying_hours', 'group', 'subject', 'room',
+        column_list = ['day_of_the_week', 'teacher', 'group', 'subject', 'room',
                        'start_time', 'end_time']
         value_list = []
         for subject in subjects:
@@ -49,10 +57,41 @@ class CLI:
             for title in column_list:
                 for elem in subject:
                     if title in elem:
-                        if 'id' in elem and title != 'room':
-                            new_list.append(fs.get_by_id(title.capitalize(), subject[elem]))
+                        if title == 'teacher':
+                            continue
+                        elif 'id' in elem and title != 'room':
+                            object = fs.get_by_id(title.capitalize(), subject[elem])
+                            if title == 'group':
+                                teacher = fs.get_by_id('Teacher', object['teacher_id'])
+                                del teacher['object_id']
+                                new_list.append(teacher)
+                                del object['teacher_id']
+                            del object['object_id']
+                            new_list.append(object)
                         else:
                             new_list.append(subject[elem])
             value_list.append(new_list)
-        print(tabulate(value_list, column_list, tablefmt='grid'))
+        print(tabulate(self.sort_days_of_the_week(value_list), column_list, tablefmt='grid'))
         self.show_menu()
+
+
+    @staticmethod
+    def sort_days_of_the_week(dicts) -> list[dict]:
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        monday = []
+        tuesday = []
+        wednesday = []
+        thursday = []
+        friday = []
+        for dct in dicts:
+            if dct[0] == days[0]:
+                monday.append(dct)
+            elif dct[0] == days[1]:
+                tuesday.append(dct)
+            elif dct[0] == days[2]:
+                wednesday.append(dct)
+            elif dct[0] == days[3]:
+                thursday.append(dct)
+            elif dct[0] == days[4]:
+                friday.append(dct)
+        return monday + tuesday + wednesday + thursday + friday
--- tests/test_lesson_row.py
+++ tests/test_lesson_row.py
@@ -1,10 +1,10 @@
 from data_model.lesson_row import LessonRow
 from tests.team_AX_class_test_feature import test_function
 
-example_1 = {'count_studying_hours': 12, 'group_id': 101, 'subject_id': 102, 'room_id': 416, 'start_time': 1230, 'end_time': 1315, 'timetable_id': 1}
-example_2 = {'count_studying_hours': 24, 'group_id': 102, 'subject_id': 105, 'room_id': 413, 'start_time': 1030, 'end_time': 1115, 'timetable_id': 1}
-example_1_update = {'count_studying_hours': 36}
-example_2_update = {'subject_id': 100238}
+example_1 = {'day_of_the_week': 'Friday', 'group_id': 1011641827322395, 'subject_id': 1081641829710171, 'room_id': 416, 'start_time': 1230, 'end_time': 1315, 'timetable_id': 1}
+example_2 = {'day_of_the_week': 'Monday', 'group_id': 1011641827322391, 'subject_id': 1081641829710191, 'room_id': 413, 'start_time': 1030, 'end_time': 1115, 'timetable_id': 1}
+example_1_update = {'day_of_the_week': 'Tuesday'}
+example_2_update = {'subject_id': 1081641829710165}
 test_function(LessonRow, example_1=example_1, example_2=example_2, example_1_update=example_1_update,
               example_2_update=example_2_update, csv_location='../data_examples/lesson_row.csv'
               )
