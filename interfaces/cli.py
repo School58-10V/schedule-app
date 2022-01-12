12f9f82f50cf5234daa318016256d6061473d9ef
Mitya <you@example.com>
2022-01-12 20:50:51 +0300

    добавил cli

--- interfaces/cli.py
+++ interfaces/cli.py
@@ -1,4 +1,6 @@
 menu = '''------------------------
+please enter number from 1 to 4(we hope u r smart enough to enter numbers not names of ur friends like Anton
+------------------------
 [1] show_timetable
 ------------------------
 [2] show_teachers
@@ -11,12 +13,12 @@ menu = '''------------------------
 
 class CLI:
     def __init__(self, user_status: str, class_of_user: str, name: str, surname: str):
-        _user_status = user_status
-        _class_of_user = class_of_user
-        _name = name
-        _surname = surname
-        identifier = []
-        __logged_in = False
+        self._user_status = user_status
+        self._class_of_user = class_of_user
+        self._name = name
+        self._surname = surname
+        self.identifier = [self.__show_timetable(), self.__show_teachers(), self.__show_classes(), self.get_user_info()]
+        self.__logged_in = False
 
     def __show_timetable(self):
         pass
@@ -25,12 +27,17 @@ class CLI:
         pass
 
     def get_user_info(self):
-        pass
+        return f"status:{self._user_status}, user's class:{self._class_of_user}, name:{self._name}, " \
+               f"surname:{self._surname}"
 
     def __show_teachers(self):
         pass
 
-    def show_menu(cls):
+    def show_menu(self):
+        print(menu)
+        choice = input()
+        self.identifier[int(choice) - 1]
+
 
 
 
