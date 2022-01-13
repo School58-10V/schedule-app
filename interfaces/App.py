af4b57d7f7fd265d4dbe102751cd8762059e45b5
Eneev <ivan.eneev@tandex.ru>
2022-01-13 16:06:25 +0300

    Доведено до рабочего состояния

--- interfaces/App.py
+++ interfaces/App.py
@@ -1,6 +1,5 @@
 from interfaces.cli import CLI
 from adapters.file_source import FileSource
 
-data = FileSource('../db')
-class_interface = CLI(data)
-CLI.run()
+class_interface = CLI(db_source=FileSource('../db'))
+class_interface.run()
