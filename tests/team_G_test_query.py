5b24b3e72c4e222d84b726496c1810207bdbd8af
Мария <kuranova.marie@yandex.ru>
2022-02-14 20:55:53 +0300

    Запрос 1 и 2

--- tests/team_G_test_query.py
+++ tests/team_G_test_query.py
@@ -4,19 +4,38 @@ import psycopg2
 conn = psycopg2.connect(dbname='schedule_app', user='kuranova',
                         password='s9iz.4c-yC-LREFf_63_', host='postgresql.aakapustin.ru')
 cursor = conn.cursor()
-
+# Запрос 2
 cursor.execute('''select count (distinct lr.object_id) from
-"Teachers" as tc
-join "TeachersForLessonRows" as tlr on tlr.teacher_id = tc.object_id
-join "LessonRows" as lr on lr.object_id = tlr.lesson_row_id
-where
-lr.day_of_the_week = 'пятница'
-and
-tc.fio = 'Антон Антонович Антонов'
-and
-lr.start_time > 1000''')
+                 "Teachers" as tc
+                 join "TeachersForLessonRows" as tlr on tlr.teacher_id = tc.object_id
+                 join "LessonRows" as lr on lr.object_id = tlr.lesson_row_id
+                 where
+                 lr.day_of_the_week = 'пятница'
+                 and
+                 tc.fio = 'Антон Антонович Антонов'
+                 and
+                 lr.start_time > 1000''')
 records = cursor.fetchall()
+
+# Запрос 1
+cursor.execute('''select distinct sb.name from
+                 "Students" as st
+                 join "StudentsForGroups" as stg on stg.student_id = st.object_id
+                 join "LessonRows" as lr on lr.group_id = stg.group_id
+                 join "TimeTables" as tb on tb.object_id = lr.timetable_id
+                 join "Subjects" as sb on lr.subject_id = sb.object_id
+                 where
+                 st.full_name = 'Бугаенко Оля Алексеевна'
+                 and
+                 tb.time_table_year = 2020
+                 ''')
+records2 = cursor.fetchall()
 
 cursor.close()
 conn.close()
-print(records)
+# Печатаем запросы.
+# Запрос 1
+print(*records)
+print()
+# Запрос 2
+print(*records2)
