23b501a67aa1e78c4b66c071da0747d7e316ed08
Мария <kuranova.marie@yandex.ru>
2022-02-22 12:10:34 +0300

    Поменяла формат даты в csv файлах. (Надо будет как-то решить, какой именно формат будем использовать)

--- tests/test_lesson.py
+++ tests/test_lesson.py
@@ -3,8 +3,10 @@ import datetime
 from data_model.lesson import Lesson
 from tests.team_AX_class_test_feature import test_function
 
-example_1 = {'start_time': 1015, 'end_time': 1100, 'day': datetime.date.today(), 'teacher_id': 5, 'group_id': 1, 'subject_id': 2, 'notes': 'Урок русского', 'state': True}
-example_2 = {'start_time': 1100, 'end_time': 1145, 'day': datetime.date.today(), 'teacher_id': 7, 'group_id': 1, 'subject_id': 1, 'notes': 'Урок математики', 'state': True}
+example_1 = {'start_time': 1015, 'end_time': 1100, 'day': datetime.date.today(), 'teacher_id': 5,
+             'group_id': 1, 'subject_id': 2, 'notes': 'Урок русского', 'state': True}
+example_2 = {'start_time': 1100, 'end_time': 1145, 'day': datetime.date.today(), 'teacher_id': 7,
+             'group_id': 1, 'subject_id': 1, 'notes': 'Урок математики', 'state': True}
 example_1_update = {'end_time': 1030}
 example_2_update = {'group_id': 3}
 test_function(Lesson, example_1=example_1, example_2=example_2, example_1_update=example_1_update,
