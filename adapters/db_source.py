d44202215bafca77b554837a8319aaa517d0f300
Mikhail Khromov <khromov05@gmail.com>
2022-02-24 19:41:16 +0300

    доделал методы get_all и get_by_id в DBSource

--- adapters/db_source.py
+++ adapters/db_source.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import psycopg2
+from psycopg2 import errorcodes
 
 
 class DBSource:
@@ -9,41 +10,46 @@ class DBSource:
     """
 
     def __init__(self, host, user, password, dbname='schedule_app'):
-        conn = psycopg2.connect(
-            dbname=dbname, user=user,
-            password=password, host=host
-        )
+        try:
+            conn = psycopg2.connect(
+                dbname=dbname, user=user,
+                password=password, host=host
+            )
+        except psycopg2.Error:
+            raise ValueError('Ошибка во время подключения к базе данных!')
         self.__conn = conn
-        self.__cursor = conn.cursor()
 
     def get_by_query(self, collection_name: str, query: dict) -> List[dict]:
         pairs = query.items()
         request = f'SELECT * FROM "{collection_name}" WHERE '
         for i in pairs:
             request += f'{i[0]}={i[1]} and '
+        cursor = self.__conn.cursor()
+        print(request)
+        self.__cursor_execute_wrapper(cursor, request, list(query.values()))
+        data = cursor.fetchall()
+        desc = cursor.description
 
-        self.__cursor.execute(request[:-5])
-        data = self.__cursor.fetchall()
-        desc = self.__cursor.description
-
-        if len(data) == 0:
-            raise ValueError(f'Объект где {", ".join([str(i[0]) + "=" + str(i[1]) for i in pairs])} не существует.')
+        # if len(data) == 0:
+        #     raise ValueError(f'Объект где {", ".join([str(i[0]) + "=" + str(i[1]) for i in pairs])} не существует.')
 
         return self.__format_tuple_to_dict(data, desc)
 
     def get_all(self, collection_name: str) -> List[dict]:
         request = f'SELECT * FROM "{collection_name}"'
-        self.__cursor.execute(request)
-        data = self.__cursor.fetchall()
-        desc = self.__cursor.description
+        cursor = self.__conn.cursor()
+        self.__cursor_execute_wrapper(cursor, request)
+        data = cursor.fetchall()
+        desc = cursor.description
 
         return self.__format_tuple_to_dict(data, desc)
 
     def get_by_id(self, collection_name: str, object_id: int) -> dict:
         request = f'SELECT * FROM "{collection_name}" WHERE object_id={object_id}'
-        self.__cursor.execute(request)
-        data = self.__cursor.fetchall()
-        desc = self.__cursor.description
+        cursor = self.__conn.cursor()
+        self.__cursor_execute_wrapper(cursor, request)
+        data = cursor.fetchall()
+        desc = cursor.description
         if len(data) == 0:
             raise ValueError(f'Объект с id {object_id} из {collection_name} не существует')
         # берем 0 индекс т.к. длина ответа всегда либо 0 (уже обработали), либо 1, т.е. смысла возвращать список нет.
@@ -71,8 +77,14 @@ class DBSource:
             to_return.append({desc[j].name: i[j] for j in range(len(desc))})
         return to_return
 
+    @classmethod
+    def __cursor_execute_wrapper(cls, cursor, request, params=None):
+        try:
+            cursor.execute(request, params)
+        except psycopg2.Error as e:
+            if errorcodes.lookup(e.pgcode) == 'UNDEFINED_TABLE':
+                raise ValueError(f'Ошибка во время выполнения запроса, таблица не существует. Запрос: {request}')
+            else:
+                raise ValueError(f'Неизвестная ошибка во время выполнения запроса, '
+                                 f'код ошибки: {errorcodes.lookup(e.pgcode)}. Запрос: {request}')
 
-# a = DBSource(user='schedule_app', password='VYRL!9XEB3yXQs4aPz_Q', host='postgresql.aakapustin.ru')
-# print(a.get_all('Students'))
-# print(a.get_by_id('Students', 122))
-# print(a.get_by_query('Groups', {'object_id': 1, 'grade': 11}))
