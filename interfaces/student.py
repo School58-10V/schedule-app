4e4473c92a810962ee7c6806332f02cbf2c51833
Mikhail Khromov <khromov05@gmail.com>
2022-03-03 18:36:05 +0300

    начал делать student interface

---
+++ interfaces/student.py
@@ -0,0 +1,28 @@
+class StudentInterface:
+    def __init__(self):
+        pass
+
+    def authorization(self):
+        username, password = input('Ваш логин'), input('Ваш пароль')
+
+    def schedule(self):
+        pass
+
+    def replacements(self):
+        pass
+
+    def free_days(self):
+        pass
+
+    def my_next_lesson(self):
+        pass
+
+    def get_class_teacher(self):
+        pass
+
+    def teacher_info(self):
+        pass
+
+
+
+
