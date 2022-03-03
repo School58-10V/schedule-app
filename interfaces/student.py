79cf2256a9f6726e511f82b2822b7f67bb165e99
vladp <Vladplanshe@gmail.com>
2022-03-03 19:27:16 +0300

    schedule

--- interfaces/student.py
+++ interfaces/student.py
@@ -6,7 +6,24 @@ class StudentInterface:
         username, password = input('Ваш логин'), input('Ваш пароль')
 
     def schedule(self):
-        pass
+        option = int(input('''
+Напишите 1 - чтобы посмотреть расписание на сегодня
+Напишите 2 - чтобы посмотреть расписание на неделю
+Напишите 3 - чтобы посмотреть расписание на какой-либо день '''))
+        while option != 1 and option != 2 and option != 3:
+            option = int(input('Введите коректное число'))
+
+        if option == 1:
+            return self.__get_schedule_for_today()
+        elif option == 2:
+            return self.__get_schedule_for_week()
+        elif option == 3:
+            day = int(input('''
+Напишите день на котороый хотите посмотреть расписание
+Понедельник - 1
+Вторник - 2
+ т.д.'''))
+            self.__get_schedule_for_day(day)
 
     def replacements(self):
         pass
@@ -23,6 +40,18 @@ class StudentInterface:
     def teacher_info(self):
         pass
 
+    def __get_schedule_for_today(self):
+        day = 1
+        return f'расписание на {day}'
+
+    def __get_schedule_for_week(self):
+        return f'расписание на неделю'
+
+    def __get_schedule_for_day(self, day):
+        return f'расписание на выбранный день'
+
+
+StudentInterface.schedule()
 
 
 
