86d9dc4389cf911cf069fb26608c910d12682fd0
Mikhail Khromov <khromov05@gmail.com>
2022-03-03 20:34:08 +0300

    доделал правки в student_interface

--- interfaces/student.py
+++ interfaces/student.py
@@ -5,15 +5,15 @@ class StudentInterface:
     def __init__(self):
         self.current_user = None
 
-    def login(self):
-        username = input('Ваше ФИО: ')
+    def __login(self):
+        username = self.__smart_input('Ваше ФИО: ')
         if self.__check_student_name(username):
             self.current_user = username
             print(f'Успешно зашли под именем {username}!')
         else:
             print(f'Такого ученика не существует.')
 
-    def logout(self):
+    def __logout(self):
         self.current_user = None
         print('Вы успешно вышли!')
 
@@ -21,137 +21,121 @@ class StudentInterface:
         while True:
             print()
             if not self.current_user:
-                self.login()
+                self.__login()
                 continue
-            option = input('Выберете опцию:\n'
+            option = self.__smart_input('Выберете опцию:\n'
                            '1. Информация о учителе\n'
                            '2. Узнать классного руководителя ученика\n'
                            '3. Мое следующее занятие\n'
                            '4. Информация о каникулах\n'
                            '5. Информация о заменах\n'
                            '6. Расписание\n'
-                           '9. Выйти из аккаунта\n'
-                           'Ваша опция: ')
+                           '0. Выйти из аккаунта\n'
+                           'Ваша опция (используйте exit чтобы в любой момент выйти в главное меню): ')
             if option == '1':
-                self.teacher_info()
+                self.__teacher_info()
             elif option == '2':
-                self.get_class_teacher()
+                self.__get_class_teacher()
             elif option == '3':
-                self.my_next_lesson()
+                self.__my_next_lesson()
             elif option == '4':
-                self.holidays()
+                self.__holidays()
             elif option == '5':
-                self.replacements()
+                self.__replacements()
             elif option == '6':
-                self.schedule()
-            elif option == '9':
-                self.logout()
+                self.__schedule()
+            elif option == '0':
+                self.__logout()
             else:
                 print('Неверная опция!')
 
-    def schedule(self):
-        option = int(input('''
-Напишите 1 - чтобы посмотреть расписание на сегодня
-Напишите 2 - чтобы посмотреть расписание на неделю
-Напишите 3 - чтобы посмотреть расписание на какой-либо день '''))
-        while option != 1 and option != 2 and option != 3:
-            option = int(input('Введите коректное число'))
+    def __schedule(self):
+        while True:
+            option = int(self.__smart_input('Напишите 1 - чтобы посмотреть расписание на сегодня\n'
+                               'Напишите 2 - чтобы посмотреть расписание на неделю\n'
+                               'Напишите 3 - чтобы посмотреть расписание на какой-либо день\n'))
+            if option in [1, 2, 3]:
+                break
+            else:
+                print('Некорректное число')
 
         if option == 1:
             print(self.__get_schedule_for_today())
         elif option == 2:
             print(self.__get_schedule_for_week())
         elif option == 3:
-            day = int(input('''
+            day = int(self.__smart_input('''
 Напишите день на котороый хотите посмотреть расписание
 Понедельник - 1
 Вторник - 2
  т.д.'''))
             print(self.__get_schedule_for_day(day))
 
-    def replacements(self):
-        v_replacements = input(
-            "Выбирите опцию: 1. Замена на сегоднишний день 2. Выбирите замену по предмету, дате, преподователю")
-        while True:
-            if v_replacements in ["1", "2"]:
-                break
-            else:
-                print("Неверная опция.")
+    def __replacements(self):
+        v_replacements = self.__smart_input(
+            "Выбирите опцию:\n"
+            "1. Мои замены на сегодняшний день\n"
+            "2. Замены по преподавателю\n")
+        if v_replacements not in ["1", "2", "exit"]:
+            print("Неверная опция.")
+            self.__replacements()
+            return
         if v_replacements == "1":
-            print(f"Замены на сегодня{self.__today_replacements()}")
+            print(f"Замены на сегодня: {self.__today_replacements()}")
         elif v_replacements == "2":
-            lesson = input(
-                "Выбирите предмет, для которого ищется замена (Если этот пораметр вас не интересует запишите '-'):")
             while True:
-                if self.__check_lesson(lesson):
-                    break
-                else:
-                    print("Неверный предмет.")
-                    lesson = input(
-                        "Выбирите предмет, для которого ищется замена (Если этот пораметр вас не интересует запишите '-'):")
-            teacher = input(
-                "Выбирите учителя, для которого ищется замена (Если этот пораметр вас не интересует запишите '-'):")
-            while True:
+                teacher = self.__smart_input("Выбирите учителя, для которого ищется замена: ")
                 if self.__check_teacher(teacher):
                     break
                 else:
                     print("Неверный учитель.")
-                    teacher = input(
-                        "Выбирите учителя, для которого ищется замена (Если этот пораметр вас не интересует запишите '-'):")
-            day = input(
-                "Выбирите день, для которого ищется замена (Если этот пораметр вас не интересует запишите '-'):")
-            while True:
-                if self.__check_day(day):
-                    break
-                else:
-                    print("Неверный день.")
-                    day = input(
-                        "Выбирите день, для которого ищется замена (Если этот пораметр вас не интересует запишите '-'):")
-            print(f"Замены на {day}: {self.__day_replacements(lesson, teacher, day)}")
+            print(f"Замены на сегодня для учителя {teacher}: {self.__replacements_by_teacher(teacher)}")
 
-    def holidays(self):
+    def __holidays(self):
         try:
-            choice = int(input('1. Выбора нужного года\n'
+            choice = int(self.__smart_input('1. Выбора нужного года\n'
                                '2. Ближайшие каникулы\n'))
         except ValueError:
-            print('Неверная опция!')
+            self.__holidays()
             return
 
         if choice == 1:
-            year = int(input('Введите нужный год.\n'))
+            year = int(self.__smart_input('Введите нужный год.\n'))
             if not self.__check_year(year):
                 while not self.__check_year(year):
-                    year = int(input('Неверный ввод. Попробуйте ввести год снова.\n'))
+                    year = int(self.__smart_input('Неверный ввод. Попробуйте ввести год снова.\n'))
             print(self.__get_holidays_for_year(year))
         elif choice == 2:
             print(self.__get_near_holidays())
         else:
             print('Неверная опция!')
+            self.__holidays()
 
-    def my_next_lesson(self):
+    def __my_next_lesson(self):
         current_datetime = datetime.datetime.now()
         closest_lesson = self.__get_closest_lesson_for_current_student(current_datetime)
         print(f'Ваш ближайший урок: {closest_lesson}')
 
-    def get_class_teacher(self):
-        student_name = input('Введите полное ФИО ученика по которому хотите получить информацию о классруке: ')
+    def __get_class_teacher(self):
+        student_name = self.__smart_input('Введите полное ФИО ученика по которому хотите получить информацию о классруке: ')
         if not self.__check_student_name(student_name):
             print('Неверное имя ученика!')
-            return
+            self.__get_class_teacher()
         print(f'Информация об учителе: {self.__get_teacher_info_by_student(student_name)}')
 
-    def teacher_info(self):
-        teacher_name = input('Введите ФИО учителя в формате И. О. Фамилия: ')
+    def __teacher_info(self):
+        teacher_name = self.__smart_input('Введите ФИО учителя в формате И. О. Фамилия: ')
         if not self.__check_teacher_name(teacher_name):
             print(f'{teacher_name} не настоящее имя!')
-            return
-        option = input('Что сделать? 1. где находится кабинет учителя 2. расписание по учителю: ')
+            self.__teacher_info()
+        option = self.__smart_input('Что сделать? 1. где находится кабинет учителя 2. расписание по учителю: ')
         if option == '1':
             print(f'Кабинет учителя: {self.__get_teacher_classroom(teacher_name)}')
         elif option == '2':
             print(f'Расписание учителя: {self.__get_teacher_schedule(teacher_name)}')
         else:
             print('Неверная опция!')
+            self.__teacher_info()
 
     def __get_teacher_info_by_student(self, student_name):
         # возвращает фио учителя-классрука для ученика у которого такое имя
@@ -188,6 +172,7 @@ class StudentInterface:
                f' - от учителя X, в кабинете N, время проведения: K'
 
     def __today_replacements(self):
+        # замены на сегодня для определенного ученика (который щас залогинен)
         return "замены на сегоднешний день"
 
     def __check_lesson(self, lesson):
@@ -208,8 +193,13 @@ class StudentInterface:
     def __get_schedule_for_day(self, day):
         return f'расписание на день {day}'
 
-    def __day_replacements(self, lesson, teacher, day):
-        return f'заменя для урока {lesson} учителя {teacher} дня {day}'
+    def __replacements_by_teacher(self, teacher):
+        return f'заменя для учителя {teacher} на сегодня'
 
-
-
+    def __smart_input(self, input_text):
+        res = input(input_text)
+        if res == 'exit':
+            # бесконечная, поэтому игнорим то что дальше будет что-то возвращено
+            print('Возвращаюсь в главное меню...')
+            self.main_loop()
+        return res
