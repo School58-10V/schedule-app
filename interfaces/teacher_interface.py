708899565b49580ba50b1473593e5ee03e8313b4
Мария <kuranova.marie@yandex.ru>
2022-03-03 19:10:59 +0300

    Добавила и закомментировала тест, внесла некоторые правки

--- interfaces/teacher_interface.py
+++ interfaces/teacher_interface.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -11,7 +12,8 @@ class TeachersInterface:
         self.teacher_id = None
         # Список, который хранит все методы для удобного вызова (его в общем, надо сюда)
         self.lst = [self.__timetable, self.__replacement, self.__student_search, self.__my_classes,
-                    self.__teacher_search, self.__my_class, self.__holidays, self.__next_lesson]  # убрал дубль self.__timetable
+                    self.__teacher_search, self.__my_class, self.__holidays,
+                    self.__next_lesson]  # убрал дубль self.__timetable
 
     def __check_input(self, string: str, message: str = None) -> str:
         #  Метод, который проверяет, что ввод корректен (не пустой хотя бы)
@@ -26,7 +28,7 @@ class TeachersInterface:
             user_input = input()
         return user_input
 
-    def __check_password(self) -> bool:
+    def __check_password(self, login: str, password: str) -> bool:
         # Метод, который проверяет, что пароль совпадает с паролем в базе
         pass
 
@@ -35,7 +37,7 @@ class TeachersInterface:
         # login = self.__check_input(login, 'Введите логин') (Проверяем на правильность)
         password = input().strip()
         # password = self.__check_input(password, 'Введите пароль')
-        if self.__check_password() is False:
+        if self.__check_password(login, password) is False:
             return None
         flag = True
         while flag:
@@ -51,19 +53,21 @@ class TeachersInterface:
                 num = int(string)  # Переводим
                 # Вызываем функцию, название которой лежит в списке (да, так оно вызываеться!!!!)
                 self.lst[num]()
-            except (IndexError, ValueError):  # Если неправильный ввод, то либо уыеличиваем счетчик
+                return True
+            except (IndexError, ValueError):  # Если неправильный ввод, то либо увеличиваем счетчик
                 if string == 'exit':  # Или другое слово, которое означает конец использования
                     print('Пока')
                     n = 3
                 else:
                     n += 1
                     print('Введите еще раз')
-            if n == 3:  # Пользователь ввел неправельные данные уже три раза!!! Забанить его!!!
+            if n == 3:  # Пользователь ввел неправильные данные уже три раза!!! Забанить его!!!
                 return False
         return True
 
     def __timetable(self):
         # Метод, который отвечает за расписание
+        # print(0, 1) для теста, чтобы как-то обозначить
         self.__timetable_method_flag = self.clever_input(['0', '1', '2'])
         if self.__timetable_method_flag == '0':
             pass
@@ -85,11 +89,17 @@ class TeachersInterface:
             pass
 
     def __replacement(self):
+        # print(2, 3)
         # Метод, который отвечает за замены
         self.__replacement_method_flag = self.clever_input(['0', '1'])
         if self.__replacement_method_flag == '0':
             pass
-        if self.__replacement_method_flag == '1':
+        elif self.__replacement_method_flag == '1':
+            pass
+        # Есть еще предложение обрабатывать это сразу в методе
+        # clever_input и пусть он возвращает тогда то, что не обрабатывается в этих методах.
+        # Тогда метод просто не будет ничего делать, а метод clever_input будет сам прощаться с пользователем
+        elif self.__student_search_method_flag == 'exit':
             pass
 
     def __my_classes(self):
@@ -109,7 +119,9 @@ class TeachersInterface:
             pass
 
     def __my_class(self):
-        self.__my_class_method_flag = self.clever_input([])
+        self.__my_class_method_flag = self.clever_input([''])  # тут пустой список передавать нельзя,
+        # т.к будет бесконечный цикл, пока пользователь не введет exit. Надо будет передавать либо пустую
+        # строку и пользователя просить нажать enter, или что-то еще придумать
         if self.__my_class_method_flag == 'exit':
             pass
 
@@ -123,6 +135,15 @@ class TeachersInterface:
             pass
 
     def __next_lesson(self):
-        self.__next_lesson_method_flag = self.clever_input([])
+        self.__next_lesson_method_flag = self.clever_input([''])  # Та же ситуация
         if self.__next_lesson_method_flag == 'exit':
             pass
+
+#
+# if __name__ == '__main__':
+#     from adapters.db_source import DBSource
+#
+#     db_source = DBSource(host='postgresql.aakapustin.ru', user='schedule_app',
+#                          password='VYRL!9XEB3yXQs4aPz_Q', dbname='schedule_app')
+#     test_intf = TeachersInterface(db_source)
+#     test_intf.run()
