ba26cd6898b3bbdbe231e3b4b77a285252898380
Мария <kuranova.marie@yandex.ru>
2022-04-04 21:29:49 +0300

    Новый метод

--- tests/http_controller_test_team_C.py
+++ tests/http_controller_test_team_C.py
@@ -1,6 +1,8 @@
 import json
 from flask import Flask, request
 
+from data_model.student import Student
+from db_source import DBSource
 
 app = Flask(__name__)
 
@@ -12,15 +14,30 @@ def hello_world():
 
 @app.route("/post", methods=["POST"])
 def hello_post():
-    return { "method": "post" }
+    return {"method": "post"}
 
 
 @app.route("/users", methods=["GET", "POST"])
 def users():
     if request.method == "GET":
-        return { "name": "main user" }
+        return {"name": "main user"}
     if request.method == "POST":
         return {
             "status": "created",
             "name": json.loads(request.get_data().decode('utf8'))['name'],
-        }
+            }
+
+
+a = DBSource(user='schedule_app', password='VYRL!9XEB3yXQs4aPz_Q', host='postgresql.aakapustin.ru')
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def get_name():
+    if request.method == 'GET':
+        return {i.get_main_id(): i.get_full_name() for i in Student.get_all(db_source=a)}
+    # if request.method
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
