48c7d8678f0da755e6e5355b13aa2fa79863fe8a
vladp <Vladplanshe@gmail.com>
2022-04-04 22:16:19 +0300

    Тестовый методы

---
+++ tests/test_team_P.py
@@ -0,0 +1,25 @@
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/main")
+def main_page():
+    return "<p>Это главная страница</p>"
+
+
+@app.route("/post", methods=["POST", "GET"])
+def hello_post():
+    data = request.get_data().decode('utf8')
+    return {"вы прислали": data}
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
