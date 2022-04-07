c491d587bd0ac642fb5c7b98bb99dd7008f856bd
Alex Afanasiev <tilzziter@gmail.com>
2022-04-07 21:17:13 +0300

    controller demo for groups

---
+++ services/db_source_factory.py
@@ -0,0 +1,24 @@
+from adapters.db_source import DBSource
+
+
+class DBFactory:
+    def __init__(self,
+                 host="postgresql.aakapustin.ru",
+                 password="VYRL!9XEB3yXQs4aPz_Q",
+                 user="schedule_app"):
+        self.__dbs = self.create_db_source(host, password, user)
+
+    def get_db_source(self):
+        return self.__dbs
+
+    def create_db_source(self,
+                         host,
+                         password,
+                         user):
+        return DBSource(host=host, password=password, user=user)
+
+    def set_db_source(self,
+                             host,
+                             password,
+                             user):
+        self.__dbs = DBSource(host=host, password=password, user=user)
