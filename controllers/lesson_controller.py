2e3265c9bd2163b08df3ae858c4a1fb75479cec2
OlgaBug <olgahaipriv7177@gmail.com>
2022-04-08 19:54:29 +0300

    методы у lesson get

--- controllers/lesson_controller.py
+++ controllers/lesson_controller.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 dbf = DBFactory()
 
 
+@app.route("/api/v1/lesson", methods=["GET"])
+def get_lessons():
+    return json.dumps([i.__dict__() for i in Lesson.get_all(dbf.get_db_source())], ensure_ascii=False)
+
+
+@app.route("/api/v1/lesson/<object_id>", methods=["GET"])
+def get_lesson_by_id(object_id):
+    return json.dumps(Lesson.get_by_id(object_id, dbf.get_db_source()).__dict__(), ensure_ascii=False)
+
+
 @app.route("/api/v1/lesson", methods=["POST"])
 def create_lesson():
     return Lesson(**request.get_json(), db_source=dbf.get_db_source()) \
