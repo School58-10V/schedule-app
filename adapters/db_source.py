a499fe4e2f5b274d616de08c1eccdf003ef53150
DragonMadness_ <supervasya45@gmail.com>
2022-04-11 11:43:00 +0300

    Добавил запросы для сутдента а также написал CRUD методы для него (метод delete_student не работает по пока что неизвестным причинам)

--- adapters/db_source.py
+++ adapters/db_source.py
@@ -112,13 +112,13 @@ class DBSource(AbstractSource):
         collection = collection_name
         if not collection_name.endswith("s"):
             collection += "s"
-        try:
-            request = f'DELETE FROM {collection} WHERE object_id = {object_id}'
-
-            self.__cursor.execute(request)
-            self.__conn.commit()
-        except Exception:
-            print("Что то пошло не так при удалении этого элемента, скорее всего виноваты внешние ключи.")
+        # try:
+        request = f'DELETE FROM {collection} WHERE object_id = {object_id}'
+        self.__cursor.execute(request)
+        self.__conn.commit()
+        # except psycopg2.Error as e:
+        #     print("Что то пошло не так при удалении этого элемента, скорее всего виноваты внешние ключи.")
+        #     raise ValueError(errorcodes.lookup(e.pgcode))
 
     @staticmethod
     def __wrap_string(value):
