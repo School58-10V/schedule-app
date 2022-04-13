5cab19ad7a5c121e32721457a234f879751db289
Мария <kuranova.marie@yandex.ru>
2022-04-13 19:39:15 +0300

    Правки

--- controllers/location_controller.py
+++ controllers/location_controller.py
@@ -9,29 +9,28 @@ dbf = DBFactory()
 
 
 # here will be your code
-@app.route("/api/v1/location/<object_id>")
+@app.route("/api/v1/location/<object_id>", methods=['PUT'])
 def update(object_id):
     try:
         Location.get_by_id(object_id, db_source=dbf.get_db_source())
     except ValueError:
         return "", 404
-    return Location(**request.get_json(), object_id=object_id, db_source=dbf.get_db_source()) \
-        .save() \
-        .__dict__()
+    return jsonify(Location(**request.get_json(), object_id=object_id,
+                            db_source=dbf.get_db_source()).save().__dict__())
 
 
 @app.route("/api/v1/location", methods=["GET"])
-def get_groups():
+def get_locations():
     return jsonify([i.__dict__() for i in Location.get_all(dbf.get_db_source())])
 
 
 @app.route("/api/v1/location/<object_id>", methods=["GET"])
-def get_group_by_id(object_id):
+def get_location_by_id(object_id):
     return jsonify(Location.get_by_id(object_id, dbf.get_db_source()).__dict__())
 
 
 @app.route("/api/v1/location", methods=["POST"])
-def create_group():
+def create_location():
     return Location(**request.get_json(), db_source=dbf.get_db_source()) \
         .save() \
         .__dict__()
@@ -39,14 +38,12 @@ def create_group():
 
 @app.route("/api/v1/location/<object_id>", methods=["DELETE"])
 def delete_location(object_id):
-    print('tyt')
     try:
         location = Location.get_by_id(object_id, dbf.get_db_source())
         location = location.delete().__dict__()
     except ValueError:
         return "", 404
     except psycopg2.Error as e:
-        print(e)
         return errorcodes.lookup(e.pgcode), 409
     return location
 
