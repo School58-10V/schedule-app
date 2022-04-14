12c04948454b73ce12f03da8023a96a91d0794aa
Юрий Снигур <yura1303@rambler.ru>
2022-04-14 20:39:17 +0300

    поправил коллекцию teacher_for_subjects

--- controllers/teacher_for_subjects_controller.py
+++ controllers/teacher_for_subjects_controller.py
@@ -1,7 +1,8 @@
-import json
+import psycopg2
+
 from data_model.teachers_for_subjects import TeachersForSubjects
 from services.db_source_factory import DBFactory
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 dbf = DBFactory()
@@ -9,20 +10,20 @@ dbf = DBFactory()
 
 @app.route("/api/v1/teacher_for_subjects", methods=["GET"])
 def get_teacher_for_subjects():
-    return json.dumps([i.__dict__() for i in TeachersForSubjects.get_all(dbf.get_db_source())], ensure_ascii=False)
+    return jsonify([i.__dict__() for i in TeachersForSubjects.get_all(dbf.get_db_source())])
 
 
 @app.route("/api/v1/teacher_for_subjects/<object_id>", methods=["GET"])
 def get_teacher_for_subjects_by_id(object_id):
-    return json.dumps(TeachersForSubjects.get_by_id(object_id, dbf.get_db_source()).__dict__(), ensure_ascii=False)
+    return jsonify(TeachersForSubjects.get_by_id(object_id, dbf.get_db_source()).__dict__())
 
 
 @app.route("/api/v1/teacher_for_subjects", methods=["POST"])
 def create_teacher_for_subjects():
-    return TeachersForSubjects(**request.get_json(), db_source=dbf.get_db_source()) \
+    return jsonify(TeachersForSubjects(**request.get_json(), db_source=dbf.get_db_source()) \
         .save() \
         .__dict__()
-
+                   )
 
 @app.route("/api/v1/teacher_for_subjects/<object_id>", methods=["PUT"])
 def update_teacher_for_subjects(object_id):
@@ -30,16 +31,20 @@ def update_teacher_for_subjects(object_id):
         TeachersForSubjects.get_by_id(object_id, db_source=dbf.get_db_source())
     except ValueError:
         return "", 404
-    return TeachersForSubjects(**request.get_json(), object_id=object_id, db_source=dbf.get_db_source()) \
+    return jsonify(TeachersForSubjects(**request.get_json(), object_id=object_id, db_source=dbf.get_db_source()) \
         .save() \
         .__dict__()
-
+            )
 
 @app.route("/api/v1/teacher_for_subjects/<object_id>", methods=["DELETE"])
 def delete_teacher_for_subjects(object_id):
-    if request.method == 'DELETE':
-        return TeachersForSubjects.get_by_id(object_id, db_source=dbf.get_db_source()).delete().__dict__()
+    try:
+        return jsonify(TeachersForSubjects.get_by_id(object_id, db_source=dbf.get_db_source()).delete().__dict__())
+    except ValueError:
+        return "", 404
+    except psycopg2.errors.ForeignKeyViolation as error:
+        return error.pgerror, 400
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run("127.0.0.1", 9000, debug=True)
