6d2fb342ed5ebd62140fba7400a21be58057b95b
DragonMadness_ <supervasya45@gmail.com>
2022-04-14 20:06:20 +0300

    Убрал все методы кроме геттеров у students_for_groups_controller.py

--- controllers/students_for_groups_controller.py
+++ controllers/students_for_groups_controller.py
@@ -1,7 +1,4 @@
-import psycopg2
-
-from flask import Flask, request, jsonify
-from psycopg2 import errorcodes
+from flask import Flask, jsonify
 
 from data_model.students_for_groups import StudentsForGroups
 from services.db_source_factory import DBFactory
@@ -21,39 +18,6 @@ def get_students_for_groups_by_id(object_id):
         return jsonify(StudentsForGroups.get_by_id(object_id, dbf.get_db_source()).__dict__())
     except ValueError:
         return jsonify(""), 404
-
-
-@app.route("/api/v1/students-for-groups", methods=["POST"])
-def create_students_for_groups():
-    try:
-        result = jsonify(StudentsForGroups(**request.get_json(), db_source=dbf.get_db_source()).save().__dict__())
-        return result
-    except TypeError:
-        return jsonify(""), 400
-
-
-@app.route("/api/v1/students-for-groups/<object_id>", methods=["PUT"])
-def update_students_for_groups(object_id):
-    try:
-        StudentsForGroups.get_by_id(object_id, dbf.get_db_source())
-        result = StudentsForGroups(**request.get_json(), db_source=dbf.get_db_source(), object_id=object_id).save().__dict__()
-        return jsonify(result)
-    except ValueError:
-        return jsonify(""), 404
-    except TypeError:
-        return jsonify(""), 400
-
-
-@app.route("/api/v1/students-for-groups/<object_id>", methods=["DELETE"])
-def delete_students_for_groups(object_id):
-    try:
-        sfg = StudentsForGroups.get_by_id(object_id, dbf.get_db_source())
-        sfg = sfg.delete().__dict__()
-    except ValueError:
-        return jsonify(""), 404
-    except psycopg2.Error as e:
-        return jsonify(errorcodes.lookup(e.pgcode)), 409
-    return jsonify(sfg)
 
 
 if __name__ == "__main__":
