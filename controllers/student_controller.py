0a4b19269d5de2d994be109284c5ffb7aa1fbc4b
DragonMadness_ <supervasya45@gmail.com>
2022-04-18 20:07:47 +0300

    фикс формата возвращаемого значения у геттеров по айди

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -2,7 +2,6 @@ import psycopg2
 from flask import Flask, request, jsonify
 from psycopg2 import errorcodes
 
-from data_model.group import Group
 from data_model.student import Student
 from data_model.students_for_groups import StudentsForGroups
 from services.db_source_factory import DBFactory
@@ -14,13 +13,14 @@ dbf = DBFactory()
 @app.route("/api/v1/student", methods=["GET"])
 def get_students():
     return jsonify([i.__dict__() for i in Student.get_all(dbf.get_db_source())])
+# TODO: add stuff here
 
 
-@app.route("/api/v1/name/get/detailed/<object_id>", methods=["GET"])
+@app.route("/api/v1/student/get/detailed/<object_id>", methods=["GET"])
 def get_student_by_id_detailed(object_id):
     try:
-        result = {"Student": Student.get_by_id(object_id, dbf.get_db_source()).__dict__(),
-                  "Groups": [group.__dict__() for group in StudentsForGroups.get_group_by_student_id(object_id, dbf.get_db_source())]}
+        result = Student.get_by_id(object_id, dbf.get_db_source()).__dict__()
+        result["groups"] = [group.__dict__() for group in StudentsForGroups.get_group_by_student_id(object_id, dbf.get_db_source())]
     except ValueError:
         return "", 404
     return jsonify(result)
@@ -29,9 +29,8 @@ def get_student_by_id_detailed(object_id):
 @app.route("/api/v1/student/<object_id>", methods=["GET"])
 def get_student_by_id(object_id):
     try:
-        result = {"Student": Student.get_by_id(object_id, dbf.get_db_source()).__dict__(),
-                  "Groups": [group_obj.get_main_id() for group_obj in
-                             StudentsForGroups.get_group_by_student_id(object_id, dbf.get_db_source())]}
+        result = Student.get_by_id(object_id, dbf.get_db_source()).__dict__()
+        result["groups_ids"] = [group_obj.get_main_id() for group_obj in StudentsForGroups.get_group_by_student_id(object_id, dbf.get_db_source())]
     except ValueError:
         return "", 404
     return jsonify(result)
