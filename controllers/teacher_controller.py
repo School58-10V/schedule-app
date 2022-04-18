d9202bf8b8a9b97d8f9b2b0907e714511acb367c
vladp <Vladplanshe@gmail.com>
2022-04-18 20:50:07 +0300

    Улучшены методы get_teachers и get_teacher_by_id

--- controllers/teacher_controller.py
+++ controllers/teacher_controller.py
@@ -10,20 +10,34 @@ app = Flask(__name__)
 dbf = DBFactory()
 
 
-@app.route("/api/v1/teacher", methods=["GET"])
+@app.route("/api/v1/teachers", methods=["GET"])
 def get_teachers():
-    return jsonify([i.__dict__() for i in Teacher.get_all(dbf.get_db_source())])
+    teachers = []
+    for i in Teacher.get_all(dbf.get_db_source()):
+        object_id = i.get_main_id()
+        teacher = Teacher.get_by_id(object_id, dbf.get_db_source()).__dict__()
+        teacher['subject_id'] = [i.get_main_id() for i in TeachersForSubjects.
+            get_subjects_by_teacher_id(object_id, db_source=dbf.get_db_source())]
+        teacher['lesson_row_id'] = [i.get_main_id() for i in TeachersForLessonRows.
+            get_lesson_rows_by_teacher_id(object_id, db_source=dbf.get_db_source())]
+        teachers.append(teacher)
+    return jsonify({"teachers": teachers})
 
 
-@app.route("/api/v1/teacher/<object_id>", methods=["GET"])
+@app.route("/api/v1/teachers/<object_id>", methods=["GET"])
 def get_teacher_by_id(object_id):
     try:
-        return jsonify(Teacher.get_by_id(object_id, dbf.get_db_source()).__dict__())
+        teacher = Teacher.get_by_id(object_id, dbf.get_db_source()).__dict__()
+        teacher['subject_id'] = [i.get_main_id() for i in TeachersForSubjects.
+            get_subjects_by_teacher_id(object_id, db_source=dbf.get_db_source())]
+        teacher['lesson_row_id'] = [i.get_main_id() for i in TeachersForLessonRows.
+            get_lesson_rows_by_teacher_id(object_id, db_source=dbf.get_db_source())]
+        return teacher
     except ValueError:
         return '', 404
 
 
-@app.route("/api/v1/teacher", methods=["POST"])
+@app.route("/api/v1/teachers", methods=["POST"])
 def create_teacher():
     try:
         return Teacher(**request.get_json(), db_source=dbf.get_db_source()).save().__dict__()
@@ -31,7 +45,7 @@ def create_teacher():
         return '', 400
 
 
-@app.route("/api/v1/teacher/<object_id>", methods=["PUT"])
+@app.route("/api/v1/teachers/<object_id>", methods=["PUT"])
 def update_teacher(object_id):
     try:
         teacher = Teacher.get_by_id(object_id, dbf.get_db_source()).__dict__()
@@ -43,7 +57,7 @@ def update_teacher(object_id):
         return '', 400
 
 
-@app.route("/api/v1/teacher/<object_id>", methods=["DELETE"])
+@app.route("/api/v1/teachers/<object_id>", methods=["DELETE"])
 def delete_teacher(object_id):
     try:
         return Teacher.get_by_id(object_id, dbf.get_db_source()).delete().__dict__()
@@ -51,26 +65,6 @@ def delete_teacher(object_id):
         return "", 404
     except psycopg2.errors.ForeignKeyViolation as error:
         return error.pgerror, 400
-
-
-@app.route("/api/v1/teacher/get_subjects/<object_id>", methods=["GET"])
-def get_subjects_by_teacher_id(object_id):
-    try:
-        dct = {'subject_id': [i.get_main_id() for i in TeachersForSubjects.
-            get_subjects_by_teacher_id(object_id, db_source=dbf.get_db_source())]}
-        return jsonify(dct['subject_id'])
-    except ValueError:
-        return '', 404
-
-
-@app.route("/api/v1/teacher/get_lesson_rows/<object_id>", methods=["GET"])
-def get_lesson_rows_by_teacher_id(object_id):
-    try:
-        dct = {'lesson_row_id': [i.get_main_id() for i in TeachersForLessonRows.
-            get_lesson_rows_by_teacher_id(object_id, db_source=dbf.get_db_source())]}
-        return jsonify(dct['lesson_row_id'])
-    except ValueError:
-        return '', 404
 
 
 if __name__ == '__main__':
