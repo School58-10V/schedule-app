8253a501b6882e3d914ef59f47d8b68a7bb950f0
Мария <kuranova.marie@yandex.ru>
2022-04-21 21:03:08 +0300

    lesson

--- controllers/lesson_controller.py
+++ controllers/lesson_controller.py
@@ -3,7 +3,7 @@ from psycopg2 import errorcodes
 
 from data_model.lesson import Lesson
 from services.db_source_factory import DBFactory
-from flask import Flask, request, jsonify, app, Response
+from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 dbf = DBFactory()
@@ -25,7 +25,9 @@ def get_lesson_by_id(object_id: int):
 
 @app.route("/api/v1/lesson", methods=["POST"])
 def create_lesson():
-    return jsonify(Lesson(**request.get_json(), db_source=dbf.get_db_source()).save().__dict__())
+    return jsonify(Lesson(**request.get_json(), db_source=dbf.get_db_source())
+                   .save()
+                   .__dict__())
 
 
 @app.route("/api/v1/lesson/<object_id>", methods=["PUT"])
@@ -34,7 +36,9 @@ def update_lessons(object_id: int):
         Lesson.get_by_id(object_id, db_source=dbf.get_db_source())
     except ValueError:
         return "", 404
-    return Lesson(**request.get_json(), object_id=object_id, db_source=dbf.get_db_source()).save().__dict__()
+    return jsonify(Lesson(**request.get_json(), object_id=object_id, db_source=dbf.get_db_source())
+                   .save()
+                   .__dict__())
 
 
 @app.route("/api/v1/lesson/<object_id>", methods=["DELETE"])
