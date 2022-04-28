3ebff9472c4bac8a640b43709f3ce8b691c20593
DragonMadness_ <supervasya45@gmail.com>
2022-04-28 21:38:46 +0300

    Поменял всё в соответствии с отзывами

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -15,11 +15,14 @@ PUBLIC_KEY = open('schedule-public.pem').read()
 @app.route('/login', methods=['POST'])
 def login():
     username, password = request.json.get('username'), request.json.get('password')
+    if not authentication(username, password):
+        return "", 401
+
     user_ip, user_agent = request.remote_addr, request.headers.get('user-agent')
-    data = {'username': username, 'password': password, 'user_ip': user_ip, 'user_agent': user_agent}
+    data = {'username': username, 'user_ip': user_ip, 'user_agent': user_agent}
 
     encoded_data = jwt.encode(data, PRIVATE_KEY, algorithm='RS256')
-    return jsonify({'ok': True, 'token': encoded_data})
+    return jsonify({'token': encoded_data})
 
 
 @app.route('/test', methods=['POST'])
@@ -29,21 +32,25 @@ def test():
 
 # Вызывается при каждом реквесте (кроме реквеста к /login)
 # Проверяет совпадение информации о пользователе с информацие из токена
-# Выбрасывает ошибку 400 когда токен некорректен
-# Выбрасывает ошибку 401 когда данные не соответствуют
+# Выбрасывает ошибку 401 когда токен некорректен
+# Выбрасывает ошибку 403 когда данные не соответствуют
 @app.before_request
 def before_request():
-    if request.url_rule.rule == '/login':
+    if request.url_rule.rule == '/login' or request.method == "GET":
         return
-    request_token = request.json.get('token')
+    request_token = request.headers.get('Authorization')
     user_ip, user_agent = request.remote_addr, request.headers.get('user-agent')
     try:
         data = jwt.decode(request_token, PUBLIC_KEY, algorithms=['RS256'])
     except DecodeError:
-        return '', 400
-    if not (data.get('user_ip') == user_ip and data.get('user_agent') == user_agent):
         return '', 401
+    if not (data.get('user_ip') == user_ip and data.get('user_agent') == user_agent):
+        return '', 403
 
+def authentication(login, password):
+    if login == "admin" and password == "password":
+        return True
+    return False
 
 if __name__ == '__main__':
     app.run()
