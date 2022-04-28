0e2b7f3b4b3e9582e47074b9797b72a232035428
Mitya <you@example.com>
2022-04-28 20:28:03 +0300

    Юзер

--- data_model/user.py
+++ data_model/user.py
@@ -1,2 +1,10 @@
-# code
+from __future__ import annotations  # нужно чтобы parse мог быть типизирован
+
+from data_model.abstract_model import AbstractModel
+from typing import List, Optional, TYPE_CHECKING
+from data_model.parsed_data import ParsedData
+import datetime
+
+if TYPE_CHECKING:
+    from adapters.db_source import DBSource
 
