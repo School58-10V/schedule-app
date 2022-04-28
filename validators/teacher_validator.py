a935667b8d3ce37080ab1e6f5e2fb7b717d44543
Юрий Снигур <yura1303@rambler.ru>
2022-04-29 00:17:08 +0300

    поправил

--- validators/teacher_validator.py
+++ validators/teacher_validator.py
@@ -1,6 +1,3 @@
-from werkzeug.exceptions import BadRequest
-
-
 class TeacherValidate:
 
     def __init__(self, request: dict, method: str):
@@ -8,46 +5,52 @@ class TeacherValidate:
         self.method = method
 
     def validate(self):
-        f = True
-        given_keys = set()
-        keys = {'bio', 'contacts', 'fio', 'office_id'}
-
-        for i in self.request.keys():
-            given_keys.add(i)
-
-        if 'lesson_row_id' in given_keys:
-            given_keys.remove('lesson_row_id')
-
-        if 'subject_id' in given_keys:
-            given_keys.remove('subject_id')
-
-        if self.method == 'PUT':
-            if 'object_id' in given_keys:
-                if type(self.request['object_id']) != int:
-                    f = False
-            else:
-                f = False
-
-        if given_keys != keys:
-            f = False
-
-        for i in self.request.keys():
-            if i == 'bio' or i == 'fio':
-                if type(self.request[i]) != str:
-                    f = False
-            if i == 'contacts':
-                if type(self.request[i]) != str and type(self.request[i]) != bool:
-                    f = False
-            if i == 'lesson_row_id' or i == 'subject_id':
-                if type(self.request[i]) == list:
-                    for g in self.request[i]:
-                        if type(g) != int:
-                            f = False
-            else:
-                f = False
-
         try:
-            if f:
-                return True
-        except BadRequest:
+            given_keys = set()
+            for i in self.request.keys():
+                given_keys.add(i)
+
+            if 'lesson_row_id' in given_keys:
+                given_keys.remove('lesson_row_id')
+
+            if 'subject_id' in given_keys:
+                given_keys.remove('subject_id')
+
+            if self.method == 'PUT':
+                if 'object_id' in given_keys:
+                    if type(self.request['object_id']) != int:
+                        raise ValueError
+                else:
+                    raise ValueError
+                given_keys.remove('object_id')
+
+            if given_keys == {'bio', 'contacts', 'fio', 'office_id'}:
+                for i in self.request.keys():
+                    if i == 'bio' or i == 'fio':
+                        if type(self.request[i]) != str:
+                            raise ValueError
+                    if i == 'contacts':
+                        if type(self.request[i]) != str and type(self.request[i]) != bool:
+                            raise ValueError
+                    if i == 'office_id':
+                        if type(self.request[i]) != int:
+                            raise ValueError
+                    if i == 'lesson_row_id':
+                        if type(self.request[i]) == list:
+                            for g in self.request[i]:
+                                if type(g) != int:
+                                    raise ValueError
+                        else:
+                            raise ValueError
+                    if i == 'subject_id':
+                        if type(self.request[i]) == list:
+                            for g in self.request[i]:
+                                if type(g) != int:
+                                    raise ValueError
+                        else:
+                            raise ValueError
+            else:
+                raise ValueError
+
+        except ValueError:
             return '', 400
