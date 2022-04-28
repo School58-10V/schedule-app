1658dcbfc60208bf1bdfb6ea6563c92adfd36e9a
Mikhail Khromov <khromov05@gmail.com>
2022-04-28 21:06:18 +0300

    сделал auth контроллер (история 3)

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,10 +1,45 @@
-from flask import Flask, request
+import jwt
+
+from flask import Flask, request, jsonify
+from jwt import DecodeError
 
 from services.db_source_factory import DBFactory
 
 app = Flask(__name__)
 dbf = DBFactory()
 
-@app.before_request
+PRIVATE_KEY = open('schedule-key.pem').read()
+PUBLIC_KEY = open('schedule-public.pem').read()
+
+
+@app.route('/login', methods=['POST'])
 def login():
-    req = request.host
+    username, password = request.json.get('username'), request.json.get('password')
+    user_ip, user_agent = request.remote_addr, request.headers.get('user-agent')
+    data = {'username': username, 'password': password, 'user_ip': user_ip, 'user_agent': user_agent}
+
+    encoded_data = jwt.encode(data, PRIVATE_KEY, algorithm='RS256')
+    return jsonify({'ok': True, 'token': encoded_data})
+
+
+@app.route('/test', methods=['POST'])
+def test():
+    return jsonify({'yay': True}), 200
+
+
+@app.before_request
+def before_request():
+    if request.url_rule.rule == '/login':
+        return
+    request_token = request.json.get('token')
+    user_ip, user_agent = request.remote_addr, request.headers.get('user-agent')
+    try:
+        data = jwt.decode(request_token, PUBLIC_KEY, algorithms=['RS256'])
+    except DecodeError:
+        return '', 400
+    if not (data.get('user_ip') == user_ip and data.get('user_agent') == user_agent):
+        return '', 401
+
+
+if __name__ == '__main__':
+    app.run()
