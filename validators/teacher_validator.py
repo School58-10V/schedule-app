4cc96d72864c34f6a37d14465c3f117f1dbb20b4
Юрий Снигур <yura1303@rambler.ru>
2022-05-05 00:24:48 +0300

    teacher validator

--- validators/teacher_validator.py
+++ validators/teacher_validator.py
@@ -6,51 +6,27 @@ class TeacherValidate:
 
     def validate(self):
         try:
-            given_keys = set()
-            for i in self.request.keys():
-                given_keys.add(i)
-
-            if 'lesson_row_id' in given_keys:
-                given_keys.remove('lesson_row_id')
-
-            if 'subject_id' in given_keys:
-                given_keys.remove('subject_id')
-
+            if self.method == 'POST':
+                if 'fio' in self.request.keys():
+                    for key in self.request.keys():
+                        if key not in {'fio', 'bio', 'contacts', 'office_id'}:
+                            raise ValueError
+                        if key == 'fio' or key == 'bio' or 'contacts':
+                            if type(self.request[key]) != str:
+                                raise ValueError
+                        if key == 'office_id':
+                            if type(self.request[key]) != int:
+                                raise ValueError
             if self.method == 'PUT':
-                if 'object_id' in given_keys:
-                    if type(self.request['object_id']) != int:
-                        raise ValueError
-                else:
-                    raise ValueError
-                given_keys.remove('object_id')
-
-            if given_keys == {'bio', 'contacts', 'fio', 'office_id'}:
-                for i in self.request.keys():
-                    if i == 'bio' or i == 'fio':
-                        if type(self.request[i]) != str:
+                if {'fio', 'object_id'} in set(self.request.keys()):
+                    for key in self.request.keys():
+                        if key not in {'fio', 'bio', 'contacts', 'office_id', 'object_id'}:
                             raise ValueError
-                    if i == 'contacts':
-                        if type(self.request[i]) != str and type(self.request[i]) != bool:
-                            raise ValueError
-                    if i == 'office_id':
-                        if type(self.request[i]) != int:
-                            raise ValueError
-                    if i == 'lesson_row_id':
-                        if type(self.request[i]) == list:
-                            for g in self.request[i]:
-                                if type(g) != int:
-                                    raise ValueError
-                        else:
-                            raise ValueError
-                    if i == 'subject_id':
-                        if type(self.request[i]) == list:
-                            for g in self.request[i]:
-                                if type(g) != int:
-                                    raise ValueError
-                        else:
-                            raise ValueError
-            else:
-                raise ValueError
-
+                        if key == 'fio' or key == 'bio' or 'contacts':
+                            if type(self.request[key]) != str:
+                                raise ValueError
+                        if key == 'office_id' or key == 'object_id':
+                            if type(self.request[key]) != int:
+                                raise ValueError
         except ValueError:
             return '', 400
