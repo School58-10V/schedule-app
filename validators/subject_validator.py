10a347897c50383df4ead70dcab71929d4e7f79f
Юрий Снигур <yura1303@rambler.ru>
2022-05-04 17:59:36 +0300

    доделал subject_validator

--- validators/subject_validator.py
+++ validators/subject_validator.py
@@ -10,9 +10,21 @@ class SubjectValidate:
                 if self.request.keys() == ['subject_name']:
                     if type(self.request['subject_name']) != str:
                         raise ValueError
+                if self.request.keys() == ['subject_name', 'teachers']:
+                    if type(self.request['subject_name']) != str or type(self.request['teachers']) != list:
+                        raise ValueError
+                    for teacher_id in self.request['teachers']:
+                        if type(teacher_id) != int:
+                            raise ValueError
             if self.method == 'PUT':
                 if set(self.request.keys()) == {'object_id', 'subject_name'}:
                     if type(self.request['object_id']) != int or type(self.request['subject_name']) != str:
                         raise ValueError
+                if set(self.request.keys()) == {'object_id', 'subject_name', 'teachers'}:
+                    if type(self.request['object_id']) != int or type(self.request['subject_name']) != str or type(self.request['teachers']) == list:
+                        raise ValueError
+                    for teacher_id in self.request['teachers']:
+                        if type(teacher_id) != int:
+                            raise ValueError
         except ValueError:
             return '', 400
