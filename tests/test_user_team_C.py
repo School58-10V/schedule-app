32877b1f25e9fe231bd05c56241762bd0f827eba
Мария <kuranova.marie@yandex.ru>
2022-05-04 20:13:16 +0300

    Дополнительная проверка

--- tests/test_user_team_C.py
+++ tests/test_user_team_C.py
@@ -2,8 +2,8 @@ from data_model.user import User
 from services.db_source_factory import DBFactory
 
 db_source = DBFactory(options="-c search_path=dbo,auth")
-new_user = User(db_source=db_source.get_db_source(), login='login1', name='name1',
-                hash_password='qwerty_password')
+new_user = User(db_source=db_source.get_db_source(), login='login2', name='name2',
+                hash_password='1qwerty_password')
 print(new_user.get_password_hash())
 new_user.password_to_hash()
 print(new_user.get_password_hash())
@@ -12,3 +12,4 @@ print(new_user.compare_hash('false_password'))
 new_user.save()
 new_user1 = User.get_by_login(login='login1', db_source=db_source.get_db_source())
 print(new_user1.__dict__())
+print(User.get_all(db_source=db_source.get_db_source()))
