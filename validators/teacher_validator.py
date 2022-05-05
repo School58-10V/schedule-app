e047f1df24bb390755503fa796e49c3867343680
Юрий Снигур <yura1303@rambler.ru>
2022-05-05 11:46:09 +0300

    поправил teacher validator

--- validators/teacher_validator.py
+++ validators/teacher_validator.py
@@ -9,7 +9,7 @@ class TeacherValidate:
             if self.method == 'POST':
                 if 'fio' in self.request.keys():
                     for key in self.request.keys():
-                        if key not in {'fio', 'bio', 'contacts', 'office_id'}:
+                        if key not in {'fio', 'bio', 'contacts', 'office_id', 'subject_id', 'lesson_row_id'}:
                             raise ValueError
                         if key == 'fio' or key == 'bio' or 'contacts':
                             if type(self.request[key]) != str:
@@ -17,10 +17,14 @@ class TeacherValidate:
                         if key == 'office_id':
                             if type(self.request[key]) != int:
                                 raise ValueError
+                        if key == 'subject_id' or key == 'lesson_row_id':
+                            for i in self.request[key]:
+                                if type(i) != int:
+                                    raise ValueError
             if self.method == 'PUT':
                 if {'fio', 'object_id'} in set(self.request.keys()):
                     for key in self.request.keys():
-                        if key not in {'fio', 'bio', 'contacts', 'office_id', 'object_id'}:
+                        if key not in {'fio', 'bio', 'contacts', 'office_id', 'object_id', 'subject_id', 'lesson_row_id'}:
                             raise ValueError
                         if key == 'fio' or key == 'bio' or 'contacts':
                             if type(self.request[key]) != str:
@@ -28,5 +32,9 @@ class TeacherValidate:
                         if key == 'office_id' or key == 'object_id':
                             if type(self.request[key]) != int:
                                 raise ValueError
+                        if key == 'subject_id' or key == 'lesson_row_id':
+                            for i in self.request[key]:
+                                if type(i) != int:
+                                    raise ValueError
         except ValueError:
             return '', 400
