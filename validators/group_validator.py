d2ec367a81f86fdb4c50f3aad37b84fc20fe7c5d
Юрий Снигур <yura1303@rambler.ru>
2022-05-05 13:04:33 +0300

    косметика

--- validators/group_validator.py
+++ validators/group_validator.py
@@ -21,16 +21,13 @@ class GroupValidate:
 
             if self.method == 'PUT':
                 if set(self.request.keys()) == {'teacher_id', 'class_letter', 'grade', 'profile_name', 'object_id'}:
-                    if type(self.request['teacher_id']) != int:
-                        raise ValueError
-                    if type(self.request['class_letter']) != str:
-                        raise ValueError
-                    if type(self.request['grade']) != int:
-                        raise ValueError
-                    if type('profile_name') != str:
-                        raise ValueError
-                    if type(self.request['object_id']) != int:
-                        raise ValueError
+                    for key in self.request.keys():
+                        if key == 'teacher_id' or key == 'grade' or key == 'object_id':
+                            if type(self.request[key]) != int:
+                                raise ValueError
+                        if key == 'class_letter' or key == 'profile_name':
+                            if type(self.request[key]) != str:
+                                raise ValueError
                 else:
                     raise ValueError
 
