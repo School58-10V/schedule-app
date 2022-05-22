b884bf520423ca6daee6081d6a8a87434358642c
Иван Шаталов <isatalov70@gmail.com>
2022-05-22 18:41:55 +0300

    fix

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -58,6 +58,8 @@ def register():
 @app.before_request
 def before_request():
     # все get реквесты и /login реквесты пропускаем, авторизация не нужна
+    if request.url_rule is None:
+        return '', 404
     if request.url_rule.rule == '/api/v1/login' or\
             request.url_rule.rule == '/api/v1/register' or\
             request.method.lower() == 'get':
