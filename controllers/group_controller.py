b27e61ea7018f5751bb6ef8069096de30a73db3d
Юрий Снигур <yura1303@rambler.ru>
2022-05-23 21:03:36 +0300

    group_transformation_service

--- controllers/group_controller.py
+++ controllers/group_controller.py
@@ -1,19 +1,20 @@
-from data_model.group import Group
 from flask import request, jsonify
 from schedule_app import app
 from validators.group_validator import GroupValidator
+from services.transformation_services.group_transformation_service import GroupTransformationService
 
+transform = GroupTransformationService()
 validator = GroupValidator()
 
 @app.route("/api/v1/group", methods=["GET"])
 def get_groups():
-    return jsonify([i.__dict__() for i in Group.get_all(app.config.get("schedule_db_source"))])
+    return jsonify(transform.get_groups_transform())
 
 
 @app.route("/api/v1/group/<object_id>", methods=["GET"])
 def get_group_by_id(object_id):
     try:
-        return jsonify(Group.get_by_id(object_id, app.config.get("schedule_db_source")).__dict__())
+        return jsonify(transform.get_group_by_id_transform(object_id))
     except ValueError:
         return '', 404
 
@@ -22,9 +23,7 @@ def get_group_by_id(object_id):
 def create_group():
     try:
         validator.validate(request.get_json(), "POST")
-        return Group(**request.get_json(), db_source=app.config.get("schedule_db_source")) \
-            .save() \
-            .__dict__()
+        return transform.create_group_transform(request.get_json())
     except ValueError:
         return "", 400
 
@@ -36,10 +35,7 @@ def update_groups(object_id):
     except ValueError:
         return "", 400
     try:
-        Group.get_by_id(object_id, db_source=app.config.get("schedule_db_source"))
-        return Group(**request.get_json(), object_id=object_id, db_source=app.config.get("schedule_db_source")) \
-            .save() \
-            .__dict__()
+        return transform.update_groups_transform(object_id, request.get_json())
     except ValueError:
         return "", 404
 
@@ -47,5 +43,4 @@ def update_groups(object_id):
 
 @app.route("/api/v1/group/<object_id>", methods=["DELETE"])
 def delete_group(object_id):
-    if request.method == 'DELETE':
-        return Group.get_by_id(object_id, db_source=app.config.get("schedule_db_source")).delete().__dict__()
+        return transform.delete_group_transform(object_id)
