3c569cb7d7b58e8d9764dfc5232af3aea84734f2
Юрий Снигур <yura1303@rambler.ru>
2022-05-23 20:22:00 +0300

    модифицировали код посредством teacher_transformation_service

--- controllers/teacher_controller.py
+++ controllers/teacher_controller.py
@@ -1,104 +1,48 @@
 import psycopg2
 
-from data_model.teacher import Teacher
-from data_model.teachers_for_subjects import TeachersForSubjects
-from data_model.teachers_for_lesson_rows import TeachersForLessonRows
 from validators.teacher_validator import TeacherValidator
+from services.transformation_services.teacher_transformation_service import TeacherTransformationService
 from flask import request, jsonify
 
 from schedule_app import app
 
+transform = TeacherTransformationService()
 validator = TeacherValidator()
 
 @app.route("/api/v1/teachers", methods=["GET"])
 def get_teachers():
-    teachers = []
-    for i in Teacher.get_all(app.config.get("schedule_db_source")):
-        teacher = i.__dict__()
-        teacher['subject_id'] = [j.get_main_id() for j in TeachersForSubjects.
-                                 get_subjects_by_teacher_id(i.get_main_id(),
-                                                            db_source=app.config.get("schedule_db_source"))]
-        teacher['lesson_row_id'] = [j.get_main_id() for j in TeachersForLessonRows.
-                                 get_lesson_rows_by_teacher_id(i.get_main_id(),
-                                                               db_source=app.config.get("schedule_db_source"))]
-        teachers.append(teacher)
-    return jsonify({"teachers": teachers})
+    return jsonify(transform.get_teachers_transform())
 
 
 @app.route("/api/v1/teachers/<object_id>", methods=["GET"])
 def get_teacher_by_id(object_id):
     try:
-
-        teacher = Teacher.get_by_id(object_id, app.config.get("schedule_db_source")).__dict__()
-        teacher['subject_id'] = [i.get_main_id() for i in TeachersForSubjects.
-                                 get_subjects_by_teacher_id(object_id,
-                                                            db_source=app.config.get("schedule_db_source"))]
-        teacher['lesson_row_id'] = [i.get_main_id() for i in TeachersForLessonRows.
-                                    get_lesson_rows_by_teacher_id(object_id,
-                                                                  db_source=app.config.get("schedule_db_source"))]
-        return jsonify(teacher)
+        return jsonify(transform.get_teacher_by_id_transform(object_id))
     except ValueError:
         return '', 404
 
 
 @app.route("/api/v1/teachers/get/detailed", methods=["GET"])
 def get_detailed_teachers():
-    teachers = []
-    for i in Teacher.get_all(app.config.get("schedule_db_source")):
-        object_id = i.get_main_id()
-        teacher = Teacher.get_by_id(object_id, app.config.get("schedule_db_source")).__dict__()
-        teacher['subject'] = [i.__dict__() for i in TeachersForSubjects.
-                              get_subjects_by_teacher_id(object_id,
-                                                         db_source=app.config.get("schedule_db_source"))]
-        teacher['lesson_row'] = [i.__dict__() for i in TeachersForLessonRows.
-                                 get_lesson_rows_by_teacher_id(object_id,
-                                                               db_source=app.config.get("schedule_db_source"))]
-        teachers.append(teacher)
-    return jsonify({"teachers": teachers})
+    return jsonify(transform.get_detailed_teachers_transform())
 
 
 @app.route("/api/v1/teachers/get/detailed/<object_id>", methods=["GET"])
 def get_teacher_detailed_by_id(object_id):
     try:
-        teacher = Teacher.get_by_id(object_id, app.config.get("schedule_db_source")).__dict__()
-        teacher['subject_id'] = [i.__dict__() for i in TeachersForSubjects.
-                                 get_subjects_by_teacher_id(object_id,
-                                                            db_source=app.config.get("schedule_db_source"))]
-        teacher['lesson_row_id'] = [i.__dict__() for i in TeachersForLessonRows.
-                                    get_lesson_rows_by_teacher_id(object_id,
-                                                                  db_source=app.config.get("schedule_db_source"))]
-        return teacher
+        return transform.get_teacher_detailed_by_id(object_id)
     except ValueError:
         return '', 404
 
 
 @app.route("/api/v1/teachers", methods=["POST"])
 def create_teacher():
-    dct = request.get_json()
     try:
-        validator.validate(dct, "POST")
-    except:
+        validator.validate(request.get_json(), "POST")
+    except ValueError:
         return '', 400
     try:
-        subject_id = dct.pop('subject_id')
-        lesson_row_id = dct.pop('lesson_row_id')
-
-        new_teacher = Teacher(**dct, db_source=app.config.get("schedule_db_source")).save()
-
-        for i in subject_id:
-            TeachersForSubjects(teacher_id=new_teacher.get_main_id(), subject_id=i,
-                                db_source=app.config.get("schedule_db_source")).save()
-
-        for i in lesson_row_id:
-            TeachersForLessonRows(teacher_id=new_teacher.get_main_id(), lesson_row_id=i,
-                                  db_source=app.config.get("schedule_db_source")).save()
-
-        new_teacher_dct = new_teacher.__dict__()
-        new_teacher_dct['subject_id'] = subject_id
-        new_teacher_dct['lesson_row_id'] = lesson_row_id
-
-        return jsonify(new_teacher_dct)
-
+        return jsonify(transform.create_teacher_transform(request.get_json()))
     except TypeError:
         return '', 400
     except ValueError:
@@ -109,28 +53,10 @@ def create_teacher():
 def update_teacher(object_id):
     try:
         validator.validate(request.get_json(), "PUT")
-    except:
+    except ValueError:
         return "", 400
     try:
-        teacher = Teacher.get_by_id(object_id, app.config.get("schedule_db_source")).__dict__()
-        teacher['subject_id'] = [i.get_main_id() for i in TeachersForSubjects.
-                                 get_subjects_by_teacher_id(object_id, db_source=app.config.get("schedule_db_source"))]
-        teacher['lesson_row_id'] = [i.get_main_id() for i in TeachersForLessonRows.
-                                    get_lesson_rows_by_teacher_id(object_id,
-                                                                  db_source=app.config.get("schedule_db_source"))]
-
-        for i in request.get_json()['subject_id']:
-            if i not in teacher['subject_id']:
-                TeachersForSubjects(teacher_id=object_id, subject_id=i,
-                                    db_source=app.config.get("schedule_db_source")).save()
-
-        for i in request.get_json()['lesson_row_id']:
-            if i not in teacher['lesson_row_id']:
-                TeachersForLessonRows(teacher_id=object_id, lesson_row_id=i,
-                                      db_source=app.config.get("schedule_db_source")).save()
-
-        return jsonify(Teacher.get_by_id(object_id, app.config.get("schedule_db_source")).__dict__())
-
+        return jsonify(transform.update_teacher_transform(object_id, request.get_json()))
     except ValueError:
         return "", 404
 
@@ -138,7 +64,7 @@ def update_teacher(object_id):
 @app.route("/api/v1/teachers/<object_id>", methods=["DELETE"])
 def delete_teacher(object_id):
     try:
-        return Teacher.get_by_id(object_id, app.config.get("schedule_db_source")).delete().__dict__()
+        return transform.delete_teacher_transform(object_id)
     except ValueError:
         return "", 404
     except psycopg2.errors.ForeignKeyViolation as error:
