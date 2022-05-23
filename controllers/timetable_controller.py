ba7cf80c174aacf5c7c9155c0562df962791390c
Юрий Снигур <yura1303@rambler.ru>
2022-05-23 20:34:45 +0300

    модифицировали код посредством teacher_transformation_service

--- controllers/timetable_controller.py
+++ controllers/timetable_controller.py
@@ -1,51 +1,48 @@
 import psycopg2
 
-from data_model.timetable import TimeTable
 from flask import request, jsonify
 from validators.timetable_validator import TimeTableValidator
+from services.transformation_services.timetable_transformation_service import TimeTableTransformationService
 
 from schedule_app import app
 
+transform = TimeTableTransformationService()
 validator = TimeTableValidator()
 
 @app.route("/api/v1/timetable", methods=["GET"])
 def get_timetable():
-    return jsonify([i.__dict__() for i in TimeTable.get_all(app.config.get("schedule_db_source"))])
+    return jsonify(transform.get_timetable_transform())
 
 
 @app.route("/api/v1/timetable/<object_id>", methods=["GET"])
 def get_timetable_by_id(object_id):
-    return jsonify(TimeTable.get_by_id(object_id, app.config.get("schedule_db_source")).__dict__())
+    return jsonify(transform.get_timetable_by_id_transform(object_id))
 
 
 @app.route("/api/v1/timetable", methods=["POST"])
 def create_timetable():
     try:
         validator.validate(request.get_json(), "POST")
-        return jsonify(TimeTable(**request.get_json(),
-                                 db_source=app.config.get("schedule_db_source")).save().__dict__())
-    except:
+        return jsonify(transform.create_timetable_transform(request.get_json()))
+    except ValueError:
         return "", 400
 
 @app.route("/api/v1/timetable/<object_id>", methods=["PUT"])
 def update_timetable(object_id):
     try:
         validator.validate(request.get_json(), "PUT")
-    except:
+    except ValueError:
         return "", 400
     try:
-        TimeTable.get_by_id(object_id, db_source=app.config.get("schedule_db_source"))
+        return jsonify(transform.update_timetable_transform(object_id, request.get_json()))
     except ValueError:
         return "", 404
-    return jsonify(TimeTable(**request.get_json(), object_id=object_id,
-                             db_source=app.config.get("schedule_db_source")).save().__dict__())
 
 
 @app.route("/api/v1/timetable/<object_id>", methods=["DELETE"])
 def delete_timetable(object_id):
     try:
-        return jsonify(
-            TimeTable.get_by_id(object_id, db_source=app.config.get("schedule_db_source")).delete().__dict__())
+        return jsonify(transform.delete_timetable_transform(object_id))
     except ValueError:
         return "", 404
     except psycopg2.errors.ForeignKeyViolation as error:
