1e089615a9c54be36822388411951266d0566c64
Мария <kuranova.marie@yandex.ru>
2022-05-24 10:37:06 +0300

    print

--- validators/teacher_validator.py
+++ validators/teacher_validator.py
@@ -11,24 +11,18 @@ class TeacherValidator:
 
         for key in required_keys:
             if key not in request.keys():
-                print(key, request.keys(), key in request.keys())
-                print('TYT')
                 raise ValueError
 
         for key in request.keys():
             if key not in allowed_keys:
-                print('tyt')
                 raise ValueError
             if key == 'fio' or key == 'bio' or key == 'contacts':
                 if type(request[key]) != str:
-                    print('fio')
                     raise ValueError
             if key == 'office_id':
                 if type(request[key]) != int:
-                    print('int')
                     raise ValueError
             if key == 'subject_id' or key == 'lesson_row_id':
                 for i in request[key]:
                     if type(i) != int:
-                        print('subject')
                         raise ValueError
