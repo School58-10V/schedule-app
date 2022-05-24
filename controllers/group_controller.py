98e05b4f8d9c4c703cf10ec654598eaa4241b24d
Мария <kuranova.marie@yandex.ru>
2022-05-24 20:14:06 +0300

    group put/post

--- controllers/group_controller.py
+++ controllers/group_controller.py
@@ -2,6 +2,8 @@ from typing import Tuple, Optional, Union
 
 from data_model.group import Group
 from flask import request, jsonify, Response
+
+from data_model.student import Student
 from schedule_app import app
 from validators.group_validator import GroupValidator
 from data_model.students_for_groups import StudentsForGroups
@@ -11,7 +13,14 @@ validator = GroupValidator()
 
 @app.route("/api/v1/group", methods=["GET"])
 def get_groups() -> Response:
-    return jsonify([i.__dict__() for i in Group.get_all(app.config.get("schedule_db_source"))])
+    dct = []
+    for i in Group.get_all(app.config.get("schedule_db_source")):
+        dct1 = i.__dict__()
+        dct1['students'] = []
+        for j in i.get_all_students():
+            dct1['students'].append(j.get_main_id())
+        dct.append(dct1)
+    return jsonify(dct)
 
 
 @app.route("/api/v1/group/<int:object_id>", methods=["GET"])
@@ -23,29 +32,59 @@ def get_group_by_id(object_id: int) -> Union[Response, Tuple[str, int]]:
 
 
 @app.route("/api/v1/group", methods=["POST"])
-def create_group() -> Union[dict, Tuple[str, int]]:
+def create_group() -> Union[Response, Tuple[str, int]]:
     try:
         validator.validate(request.get_json(), "POST")
-        return Group(**request.get_json(), db_source=app.config.get("schedule_db_source")) \
-            .save() \
-            .__dict__()
+        dct = request.get_json()
+        student = []
+        if 'students' in dct:
+            student = dct.pop('students')
+        group = Group(**request.get_json(), db_source=app.config.get("schedule_db_source")) \
+            .save()
+        for i in student:
+            student1 = Student.get_by_id(i, db_source=app.config.get('schedule_db_source'))
+            group.append_student(student1)
+        dct['students'] = student
+        dct['object_id'] = group.get_main_id()
+        return jsonify(dct)
     except ValueError:
         return "", 400
 
 
 @app.route("/api/v1/group/<int:object_id>", methods=["PUT"])
-def update_groups(object_id: int) -> Union[Tuple[str, int], dict]:
+def update_groups(object_id: int) -> Union[Tuple[str, int], Response]:
+    if request.get_json().get('object_id') != object_id:
+        return "", 400
     try:
         validator.validate(request.get_json(), "PUT")
     except ValueError:
         return "", 400
     try:
-        Group.get_by_id(object_id, db_source=app.config.get("schedule_db_source"))
-        return Group(**request.get_json(), object_id=object_id, db_source=app.config.get("schedule_db_source")) \
-            .save() \
-            .__dict__()
+        group = Group.get_by_id(object_id, db_source=app.config.get("schedule_db_source"))
     except ValueError:
         return "", 404
+    dct = request.get_json()
+    student_id = []
+    if 'students' in dct:
+        student_id = dct.pop('students')
+    old_students_id = [i.get_main_id() for i in group.get_all_students()]
+    delete_students = list(set(old_students_id) - set(student_id))
+    add_students = list(set(student_id) - set(old_students_id))
+    try:
+        for i in delete_students:
+            student1 = Student.get_by_id(i, db_source=app.config.get('schedule_db_source'))
+            group.remove_student(student1)
+
+        for i in add_students:
+            student1 = Student.get_by_id(i, db_source=app.config.get('schedule_db_source'))
+            group.append_student(student1)
+
+    except ValueError:
+        return '', 400
+
+    group = Group(**dct, db_source=app.config.get('schedule_db_source')).save()
+    dct['students'] = student_id
+    return jsonify(dct)
 
 
 @app.route("/api/v1/group/<int:object_id>", methods=["DELETE"])
