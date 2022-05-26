b9e27fd4fed95523e0ed2b1e86c555b54d243669
Mikhail Khromov <khromov05@gmail.com>
2022-05-26 19:57:38 +0300

    пофиксил validator_factory.py, сделал snake_case_2_camel_case() и пофискил один нейминг

--- services/validator_factory.py
+++ services/validator_factory.py
@@ -7,7 +7,7 @@ from validators.no_learning_period_validator import NoLearningPeriodValidator
 from validators.student_validator import StudentValidator
 from validators.subject_validator import SubjectValidator
 from validators.teacher_validator import TeacherValidator
-from validators.timetable_validator import TimeTableValidator
+from validators.timetable_validator import TimetableValidator
 
 
 class ValFactory:
@@ -21,14 +21,20 @@ class ValFactory:
         self.validators.append(StudentValidator())
         self.validators.append(SubjectValidator())
         self.validators.append(TeacherValidator())
-        self.validators.append(TimeTableValidator())
+        self.validators.append(TimetableValidator())
+
+    @classmethod
+    def snake_case_2_camel_case(cls, snake_case_text: str):
+        """ converts camel_case_name to CamelCaseName """
+        return ''.join([i.capitalize() for i in snake_case_text.split('_')])
 
     def get_appropriate_validator(self, name):
-        name = name.replace("_controller", "Validator")
-        name.capitalize()
+        file_name = name.split('.')[-1]
+        camel_case_name = self.snake_case_2_camel_case(file_name)
+        camel_case_name = camel_case_name.replace("Controller", "Validator")
+        # name.capitalize()
+
         for validator in self.validators:
-            if validator.get_name() == name:
-                return
+            if validator.get_name() == camel_case_name:
+                return validator
         raise ValueError("Неверный тип валидатора")
-
-
--- validators/timetable_validator.py
+++ validators/timetable_validator.py
@@ -1,11 +1,11 @@
 import validators.abstract_validator
 
 
-class TimeTableValidator(validators.abstract_validator.AbstractValidator):
+class TimetableValidator(validators.abstract_validator.AbstractValidator):
     def __init__(self):
         required_keys = {'time_table_year'}
         allowed_keys = {'time_table_year'}
 
         keys_types = {'time_table_year': int}
 
-        super(TimeTableValidator, self).__init__(required_keys, allowed_keys, keys_types)
+        super(TimetableValidator, self).__init__(required_keys, allowed_keys, keys_types)
