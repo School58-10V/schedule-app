8a81f1cba1a38ff67955c4923ae31f52d4a05283
DragonMadness <supervasya45@gmail.com>
2022-09-12 22:29:53 +0300

    Started moving stuff to LessonRow frame

--- tests/lexey/functions.py
+++ tests/lexey/functions.py
@@ -1,8 +1,8 @@
 import random
-from datetime import date
-from data_model.lesson import Lesson
+from data_model.lesson_row import LessonRow
 from data_model.subject import Subject
 from data_model.teacher import Teacher
+from data_model.student import Student
 
 def get_random_student(students):
     return students[random.randrange(0, len(students))]
@@ -15,18 +15,16 @@ def get_random_teachers(teachers):
 def get_random_subject(subjects):
     return subjects[random.randrange(0, len(subjects))]
 
-
-def create_random_lesson(source, group, subjects, teachers, day, start_time, end_time):
-    Lesson(source, start_time, end_time, date(2022, 9, day), group_id=group.get_main_id(),
-           subject_id=get_random_subject(subjects).get_main_id(),
-           teacher_id=get_random_teachers(teachers).get_main_id(), notes="test").save()
+def create_random_lesson_row(source, group, subject, teacher, day, start_time, end_time):
+    return LessonRow(source, day, start_time=start_time, end_time=end_time, group_id=group.get_main_id(), subject_id=subject,
+              room_id=teacher.get_office_id(), timetable_id=41).save().append_teacher(teacher)
 
 
 def create_random_day(source, group, subjects, teachers, day, pair_amount):
     start_time = 900
     end_time = 945
     for i in range(pair_amount * 2):
-        create_random_lesson(source, group, subjects, teachers, day, start_time, end_time)
+        create_random_lesson_row(source, group, get_random_subject(subjects), get_random_teachers(teachers), day, start_time, end_time)
         if i % 2 == 0:
             breaktime = 10
         if i % 2 != 0:
@@ -60,8 +58,19 @@ def get_data(source, group):
     print(source.get_by_query(collection_name="Lesson", query={"group_id": group.get_main_id()}))
 
 def get_rand_dates(dates_amount=3):
-    dates = list(range(13, 17))
+    dates = list(range(0, 6))
     result = []
     for i in range(dates_amount):
         result.append(dates.pop(random.randrange(0, len(dates))))
     return result
+
+def process_students(source, students, group):
+    people = {}
+    req_names = {"Алексей": "2005-06-23", "Василий": "2005-06-21", "Иван": "2005-04-19"}
+    for student in students:
+        if student.get_name() in req_names and student.get_name() not in people:
+            people[student.get_name()] = student
+    for name in req_names:
+        if name not in people:
+            people[name] = Student(source, name, req_names[name], bio="пельмень").save().append_group(group)
+    return list(people.values())
--- tests/lexey/main.py
+++ tests/lexey/main.py
@@ -1,7 +1,5 @@
 from services.db_source_factory import DBFactory
 from config import Configuration
-from data_model.group import Group
-from data_model.student import Student
 from tests.lexey import functions
 
 cfg = Configuration("../../config.json")
@@ -13,23 +11,9 @@ dbf.set_db_source(host=cfg.get_configuration()["schedule_database"]["host"],
 
 source = dbf.get_db_source()
 
-group = Group(source, 4, "Б", 11, "математика")
-group.save()
-
-alex = Student(source, "Алексей", "2005-06-23", bio="пельмень")
-alex.save()
-alex.append_group(group)
-
-vasya = Student(source, "Василий", "2005-06-21", bio="пельмень")
-vasya.save()
-vasya.append_group(group)
-
-vanya = Student(source, "Иван", "2005-04-19", bio="пельмень")
-vanya.save()
-vanya.append_group(group)
-
+group = source.get_by_id("Groups", 55)
+students = functions.process_students(source, source.get_all("Students"), group)
 subjects = functions.parse_subjects(source, source.get_all("Subjects"))
-
 teachers = functions.parse_teachers(source, source.get_all("Teachers"))
 
 dates = functions.get_rand_dates()
