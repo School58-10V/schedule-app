62a18fab65d7d97d6781cd97eab11a07f783bc7e
Eneev <ivan.eneev@tandex.ru>
2022-09-12 20:07:00 +0300

    Добавление get-методов.

--- tests/lexey/main.py
+++ tests/lexey/main.py
@@ -2,14 +2,13 @@ from services.db_source_factory import DBFactory
 from config import Configuration
 from data_model.group import Group
 from data_model.student import Student
-import functions
-
+from tests.lexey import functions
 
 cfg = Configuration()
 dbf = DBFactory().set_db_source(host=cfg.get_configuration()["schedule_database"]["host"],
-                           password=cfg.get_configuration()["schedule_database"]["password"],
-                           user=cfg.get_configuration()["schedule_database"]["user"],
-                           options=cfg.get_configuration()["schedule_database"]["options"])
+                                password=cfg.get_configuration()["schedule_database"]["password"],
+                                user=cfg.get_configuration()["schedule_database"]["user"],
+                                options=cfg.get_configuration()["schedule_database"]["options"])
 
 source = dbf.get_db_source()
 
@@ -28,7 +27,6 @@ vanya = Student(source, "Иван", "2005-04-19", bio="пельмень")
 vanya.save()
 vanya.append_group(group)
 
-
 subjects = functions.parse_subject(source, source.get_all("Subjects"))
 
 teachers = functions.parse_teacher(source, source.get_all("Teachers"))
@@ -42,4 +40,3 @@ functions.create_random_day(source, group, subjects, teachers, 8, thursday_pairs
 functions.create_random_day(source, group, subjects, teachers, 9, friday_pairs)
 
 functions.get_data(source, group)
-
