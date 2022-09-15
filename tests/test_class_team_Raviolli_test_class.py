ae405f67cbe6a9f5a6f5bd569d90359ced06672f
Мария <kuranova.marie@yandex.ru>
2022-09-15 20:17:54 +0300

    Красивый вывод

--- tests/test_class_team_Raviolli_test_class.py
+++ tests/test_class_team_Raviolli_test_class.py
@@ -6,4 +6,8 @@ db_source = DBSource(host="postgresql.aakapustin.ru",
                      user="schedule_app",
                      options="-c search_path=dbo,public")
 test_class = TestClass(db_source=db_source)
-print(test_class.run(lst=[2, 2, 4], class_letter='Raviolli', grade=11))
+lst = test_class.run(lst=[2, 2, 4], class_letter='Raviolli', grade=11)
+for i in sorted(list(lst.keys())):
+    print(i)
+    print(*lst[i], sep='\n')
+    print()
