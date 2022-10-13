dc70b38f57d8f69f9fb02ee62c140a4f664401e6
Мария <kuranova.marie@yandex.ru>
2022-10-13 21:09:15 +0300

    Лишние импорты

--- frontend/views.py
+++ frontend/views.py
@@ -1,15 +1,7 @@
-from flask import request, render_template
+from flask import render_template
 
-from db_source import DBSource
 from schedule_app import app
-
-from interfaces.student_interface import StudentInterface
 from interfaces.schedule_interface import get_schedule_for_today
-
-DBSOURCE = DBSource(host="postgresql.aakapustin.ru",
-                    password="VYRL!9XEB3yXQs4aPz_Q",
-                    user="schedule_app",
-                    options="-c search_path=dbo,public")
 
 
 BASE_PATH = '127.0.0.1:5000/api/v1'
@@ -17,7 +9,8 @@ BASE_PATH = '127.0.0.1:5000/api/v1'
 
 @app.route('/', methods=['GET'])
 def main_page():
-    return render_template('main.html', schedule=get_schedule_for_today(db_source=DBSOURCE, current_user_id=119))
+    return render_template('main.html', schedule=get_schedule_for_today(db_source=app.config.get('schedule_db_source'),
+                                                                        current_user_id=119))
 
 
 @app.route('/login', methods=['GET'])
