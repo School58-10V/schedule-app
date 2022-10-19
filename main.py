a13d864a667be0bd848a522f74cc15d71a7c047c
Mikhail Khromov <khromov05@gmail.com>
2022-10-19 23:04:05 +0300

    ура ура .js теперь будет работать на локалхосте

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import controllers
 import web
 from schedule_app import app
+
+import mimetypes
+
+mimetypes.add_type('application/javascript', '.js')
+mimetypes.add_type('text/css', '.css')
+
 app.run(debug=True)
