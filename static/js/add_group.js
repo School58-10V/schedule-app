438a5929d8ecbfaad78ff6389c44a0e55fc21ef0
Mityun <mitki.live@gmail.com>
2022-10-26 16:22:00 +0300

    add_group, но эта шайтан машина не работает

--- static/js/add_group.js
+++ static/js/add_group.js
@@ -1,31 +1,35 @@
 import { BASE_PATH } from './config.js'
 import { setCookie } from './usefulFunctions.js'
 
-
 sendButton.addEventListener('click', (e)=>{
     e.preventDefault();
     alert('123');
     let request = new XMLHttpRequest();
-    let url = BASE_PATH + '/teachers';
+    let url = BASE_PATH + '/groups';
     request.open('POST', url);
 //    let lesson_rows = lesson_row_id.value;
 //
+
+    let id = document.getElementById("id").value
+
     let data = JSON.stringify({
-        "fio": fio.value,
-        "bio": bio.value,
-        "contacts": contacts.value,
-        "office_id": office_id.value,
+        "id": document.getElementById("id").value,
+        "grade": document.getElementById("grade").value,
+        "profile_name": document.getElementById("profile_name").value,
+        "students": document.getElementById("students").value,
+        "teacher": document.getElementById("teacher").value
     });
     request.withCredentials = true;
     request.setRequestHeader("Content-Type", "application/json");
 
     request.send(data);
-    fio.value = '';
-    bio.value = '';
-    contacts.value = '';
-    office_id.value = '';
+    id.value = '';
+    grade.value = '';
+    profile_name.value = '';
+    students.value = '';
+    teacher.value = '';
     request.onload = () => {
-        if(request.status == 200){
+        if(request.status === 200){
             let response = JSON.parse(request.response);
 
             let token = response['token'];
@@ -38,7 +42,7 @@ sendButton.addEventListener('click', (e)=>{
 
             window.location.pathname = '/';
         }
-        else if (request.status == 401){
+        else if (request.status === 401){
             loginField.style.borderColor = 'red';
             passwordField.style.borderColor = 'red';
             loginField.placeholder = 'Неверные данные';
