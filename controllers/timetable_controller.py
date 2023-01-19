19c40d40bf9bbab86fe414a5d995f168e59d7e69
maVovk <MichaelVovk@yandex.ru>
2023-01-19 18:14:01 +0300

    Первая версия парсера расписания + requirements.txt updated

--- controllers/timetable_controller.py
+++ controllers/timetable_controller.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
+
+import io
 from typing import TYPE_CHECKING
 
 import psycopg2, logging
 from flask import request, jsonify
+
+import numpy as np
+import pandas as pd
+import itertools
 
 from schedule_app import app
 from data_model.timetable import TimeTable
@@ -47,7 +53,6 @@ def create_timetable() -> Union[Response, Tuple[str, int]]:
         return "", 500
 
 
-
 @app.route("/api/v1/timetable/<int:object_id>", methods=["PUT"])
 def update_timetable(object_id: int) -> Union[Tuple[str, int], Response]:
     if request.get_json().get('object_id') != object_id:
@@ -84,11 +89,98 @@ def delete_timetable(object_id: int) -> Union[Response, Tuple[str, int], Tuple[A
         return "", 500
 
 
+def parse_class_lesson(information):
+    return tuple(map(str.strip, information))
+
+
+def parse_group_lesson(information):
+    subject_name = [information[0].strip()]
+    teacher_names = [information[1].strip()]
+    classrooms = []
+
+    for el in information[2:-1]:
+        classroom, *teacher = el.strip().split(' ')
+
+        classrooms.append(classroom)
+        teacher_names.append(' '.join(teacher))
+
+    classrooms.append(information[-1].strip())
+
+    subjects = list(itertools.zip_longest(subject_name, teacher_names, classrooms, fillvalue=subject_name[0]))
+
+    return subjects
+
+
+def tuple_to_dict(keys: list, values: tuple):
+    return dict(zip(keys, values))
+
+
+def check_if_group_lesson(data):
+    return type(data) == list
+
+
+def parse_and_set_lesson(i, j, el, df):
+    try:
+        np.isnan(el)  # проверяем, пропуск или нет
+
+        if i != 0 and i % 2 == 1:
+            df.iloc[i, j] = df.iloc[i - 1, j]
+        elif j != 0 and check_if_group_lesson(df.iloc[i, j - 1]):
+            df.iloc[i, j] = df.iloc[i, j - 1]
+
+    except TypeError:
+        information = el.split(',')
+
+        if len(information) == 3:
+            lesson = parse_class_lesson(information)
+        else:
+            lesson = parse_group_lesson(information)
+
+        df.iloc[i, j] = lesson
+
+
+def parse_parallel(columns, shift, df):
+    for j, col in enumerate(columns, 2 + shift):
+        for i, el in enumerate(df[col]):
+            parse_and_set_lesson(i, j, el, df)
+
+    return df
+
+
+def parse_day(df):
+    shift = 0
+    for parallel in range(7, 11 + 1):
+        columns = [col for col in df.columns if str(parallel) in col]
+        df = parse_parallel(columns, shift, df)
+        shift += len(columns)
+
+    return df
+
+
 @app.route('/api/v1/timetable/upload', methods=['POST'])
 def upload_files():
     data = request.files['file']
-    print(data)
-    print(type(data))
-    data.save('name.xlsx')
-    return '', 200
+
+    try:
+        print(data)
+        print(type(data))
+
+        # xls = pd.ExcelFile(io.BytesIO(data))
+        xls = pd.ExcelFile(data)
+        sheet_to_df = pd.read_excel(xls, sheet_name=None)
+
+        for el in sheet_to_df:
+            df = sheet_to_df[el]
+
+            df.columns = df.iloc[0].values
+            df.rename(columns={np.nan: 'Время звонков'}, inplace=True)
+            df.drop(0, inplace=True)
+
+            df = parse_day(df)
+
+            print(f'Получили и успешно спарсили расписание на один день с листа {el}')
+
+        return '', 200
+    except Exception as e:
+        return e, 500
 
