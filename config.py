82cf38bfc9e62d80af1b9ec1f22f63a28e4d6e11
DragonMadness <supervasya45@gmail.com>
2023-03-15 21:39:15 +0300

    added convenience methods to config

--- config.py
+++ config.py
@@ -9,3 +9,19 @@ class Configuration:
 
     def get_configuration(self):
         return self.__cfg
+    
+    def get(self, path: str) -> str:
+        points = path.split(".")
+        section = self.__cfg.copy()
+        for point in points:
+            if point in section:
+                section = section[point]
+            else:
+                raise ValueError("This path does not exist!")
+        return section
+    
+    def getBoolean(self, path: str):
+        return True if self.get(path) == "True" else False
+    
+    def getInt(self, path: str):
+        return int(self.get(path))
